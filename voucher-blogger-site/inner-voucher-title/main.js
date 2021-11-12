19777aa19ad0f984efad492845b31209dea6c919
Cuongbok <92067448+cuongbokreal@users.noreply.github.com>
2021-11-12 13:58:43 +0700

    Update main.js

--- voucher-blogger-site/inner-voucher-title/main.js
+++ voucher-blogger-site/inner-voucher-title/main.js
@@ -1,1 +1,29 @@
+//custom tí background cho ngựa ngựa =-))
+var listBgColor = ["ffeeee",//hồng nhạt
+    "#eee3ff",//tím hơi nhạt
+    "#b7b6e4",//tím hơi đậm
+    "#ffe3eb",//hồng hơi nhạt
+    "#c5ce67",//xanh cức
+    "#f38b8b",//đỏ tươi
+    "#8bcff3",//xanh facebook
+    ]
+randomBgColor = listBgColor[Math.floor(Math.random() * listBgColor.length)];
+document.getElementsByClassName('voucher-main')[0].style.background = randomBgColor;
 
+//mariquee
+function createMariquee(){
+    var surf = document.getElementById('surf');
+    var innerVouchers = document.getElementById('innerVouchers');
+    setTimeout(function(){ 
+        var titleVouchers = document.getElementsByClassName('title');
+        let innerSurf = "";
+        for(let i=3;i<titleVouchers.length;i++){
+            innerSurf+=`<a class="vouchers-tag-link" href="?ctv=vouchers_click&utm_source=vouchers_a_tag_click&utm_medium=${titleVouchers[i].innerText}#couponTab" title="Mã tại TrumGiamGia: ${titleVouchers[i].innerText}" alt="Mã tại TrumGiamGia: ${titleVouchers[i].innerText}">${titleVouchers[i].innerText}</a>   |   `
+        }
+        if(pathNameVoucherSite = '/'){
+            surf.innerHTML = innerSurf;
+            innerVouchers.innerHTML = innerSurf;
+        }    
+    }, 5000);
+}    
+createMariquee();
