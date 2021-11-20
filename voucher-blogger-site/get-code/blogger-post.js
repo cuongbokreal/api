ec3a5c904cccbfe9d515dd17f683da179c79fdea
Cuongbok <92067448+cuongbokreal@users.noreply.github.com>
2021-11-20 17:31:41 +0700

    Update blogger-post.js

--- voucher-blogger-site/get-code/blogger-post.js
+++ voucher-blogger-site/get-code/blogger-post.js
@@ -29,9 +29,6 @@ var listAd = [
  ];
 
     var aMainCode = document.querySelector('#mainCode');
-
-if(p.length > 0){aMainCode.setAttribute("data-href", `${aMainCode.getAttribute("data-href")}?utm_campaign=${listAd[p].id}`);}
-
     var mainCode = document.querySelector('#mainCode > .code-text > input');
     var copyCodeButton = document.querySelector('#mainCode > span');
     var NotifyCopiedVoucherCode = document.getElementById('NotifyCopiedVoucherCode');
