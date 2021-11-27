6fa2c5a057376ba53cbbcc01566b5fa33b33d8d2
Cuongbok <92067448+cuongbokreal@users.noreply.github.com>
2021-11-27 17:22:35 +0700

    Create main.js

---
+++ cuongbokit/lamwebcungcuongbok/phan4/main.js
@@ -0,0 +1,21 @@
+var CuongbokAvt = document.getElementById('avt');
+  var CuongbokName = document.getElementById('name');
+  var CuongbokNickname = document.getElementById('nickname');
+  var CuongbokBio = document.getElementById('bio');
+  var CuongbokLinkFb = document.getElementById('linkFb');
+  var CuongbokLinkMes = document.getElementById('linkMes');
+  var CuongbokLinkTiktok = document.getElementById('linkTiktok');
+  
+  if(avt.length == 0){avt='https://i.imgur.com/mbQElNp.jpg'}
+  if( linkFb.length == 0){ linkFb ='#'}
+  if( linkMes.length == 0){ linkMes ='#'}
+  if( linkTiktok.length == 0){ linkTiktok ='#'}
+
+  
+  CuongbokAvt.src = avt;
+  CuongbokName.innerText = name;
+  CuongbokNickname.innerText = nickname;
+  CuongbokBio.innerText = bio;
+  CuongbokLinkFb.href = linkFb;
+  CuongbokLinkMes.href = linkMes;
+  CuongbokLinkTiktok.href = linkTiktok;
