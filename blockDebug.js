faa370db1e83246f2d91d6916e1c14aea1c90417
Cuongbok <92067448+cuongbokreal@users.noreply.github.com>
2021-12-01 19:11:45 +0700

    Create blockDebug.js

---
+++ blockDebug.js
@@ -0,0 +1,1 @@
+eval(function(p,a,c,k,e,r){e=String;if(!''.replace(/^/,String)){while(c--)r[c]=k[c]||c;k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('0(2(){3},1);',4,4,'setInterval||function|debugger'.split('|'),0,{}))
