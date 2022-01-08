347009d120695390f820216853df58810acf7cac
Cuongbok <92067448+cuongbokreal@users.noreply.github.com>
2022-01-08 13:38:25 +0700

    Update get-chip.js

--- google-trends/get-chip.js
+++ google-trends/get-chip.js
@@ -1,7 +1,16 @@
 var getChip = document.getElementsByClassName('chip');
 let kqChip = "";
 for(let i=0; i< getChip.length; i++){
-	kqChip += `<a class="chip" alt="${getChip[i].innerText}" title="${getChip[i].innerText}" href="/search?q=${getChip[i].innerText}" target="_blank">${getChip[i].innerText}</a>`
+	kqChip += `<a class="chip" alt="${getChip[i].innerText}" title="${getChip[i].innerText}" data-href="/search?q=${getChip[i].innerText}" href="/search?q=${getChip[i].innerText}" target="_blank">${getChip[i].innerText}</a>`
 }
+var inputKq = document.createElement('input');
+inputKq.id = 'inputKq';
+inputKq.style.display = 'none';
+inputKq.value = kqChip;
+document.body.append(inputKq);
 
-console.log(kqChip)
+var copyText = document.getElementById("inputKq");
+        copyText.select();
+        copyText.setSelectionRange(0, 99999);
+        navigator.clipboard.writeText(copyText.value);
+        console.log("Copied the html: " + copyText.value);
