96752e3751f8990f02bb0ab3333c410be1318266
Cuongbok <92067448+cuongbokreal@users.noreply.github.com>
2022-01-08 14:00:15 +0700

    Update get-chip.js

--- google-trends/get-chip.js
+++ google-trends/get-chip.js
@@ -10,8 +10,10 @@ for(let i=0; i< getChip.length; i++){
 }
 inputKq.value = kqChip;
 
-var copyText = document.getElementById("inputKq");
+setTimeout(function(){
+	var copyText = document.getElementById("inputKq");
         copyText.select();
         copyText.setSelectionRange(0, 99999);
         navigator.clipboard.writeText(copyText.value);
         console.log("Copied the html: " + copyText.value);
+},500);
