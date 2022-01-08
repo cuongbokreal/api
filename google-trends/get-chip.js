18cc1ec208e5933df3df5edb8a610a883453ef9b
Cuongbok <92067448+cuongbokreal@users.noreply.github.com>
2022-01-08 14:01:16 +0700

    Update get-chip.js

--- google-trends/get-chip.js
+++ google-trends/get-chip.js
@@ -15,5 +15,5 @@ setTimeout(function(){
         copyText.select();
         copyText.setSelectionRange(0, 99999);
         navigator.clipboard.writeText(copyText.value);
-        console.log("Copied the html: " + copyText.value);
+        console.log(copyText.value);
 },500);
