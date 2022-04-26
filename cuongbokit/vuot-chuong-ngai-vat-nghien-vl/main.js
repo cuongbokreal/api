62406587a6be4b2c961b3e145eaa12971d7c8d2e
Cuongbok <92067448+cuongbokreal@users.noreply.github.com>
2022-04-26 18:54:59 +0700

    Update main.js

--- cuongbokit/vuot-chuong-ngai-vat-nghien-vl/main.js
+++ cuongbokit/vuot-chuong-ngai-vat-nghien-vl/main.js
@@ -1,8 +1,16 @@
+document.body.innerHTML += `
+<link href="https://cuongbokreal.github.io/api/cuongbokit/vuot-chuong-ngai-vat-nghien-vl/menu.css" rel="stylesheet"/>
+<script src='https://code.jquery.com/jquery-2.2.4.min.js'></script>
+<style>
+#actKq{position:fixed; bottom:0; right:0;}
+#kqZone{position:fixed; top:75px; right:0; max-width:100%;}
+#kq{position:fixed; top:0; right:0; max-width:100%;}
+</style>
+`;
+
 var goiY = document.querySelectorAll('.app-tooltip > div > div')
 
 var kqZone = `
-<link href="https://cuongbokreal.github.io/api/cuongbokit/vuot-chuong-ngai-vat-nghien-vl/menu.css" rel="stylesheet"/>
-
 <div id="kq">
   <div id="hamburger" class="close">
     <span></span>
@@ -15,13 +23,6 @@ var kqZone = `
     </ul>
   </div>
 </div>
-<style>
-#actKq{position:fixed; bottom:0; right:0;}
-#kqZone{position:fixed; top:0; right:0; max-width:100%;}
-#kq{position:fixed; top:0; right:0; max-width:100%;}
-</style>
-
-<script src='https://code.jquery.com/jquery-2.2.4.min.js'></script>
 `;
 document.body.innerHTML += kqZone;
 var getKqZone = document.getElementById('kqZone');
@@ -39,3 +40,4 @@ $(function () {
 });
 
 ketqua()
+
