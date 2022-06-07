78d807899158e1049c5b91152fde2cf37120cabc
Cuongbok <92067448+cuongbokreal@users.noreply.github.com>
2022-06-07 13:15:47 +0700

    Update main.js

--- cuongbokit/extension/fix-blur-thumbnail/main.js
+++ cuongbokit/extension/fix-blur-thumbnail/main.js
@@ -1,13 +1,12 @@
 setTimeout(replaceThumbnail,5000)
 
 function replaceThumbnail(){
-if (document.querySelectorAll('.itemThumbnail > a > div')){
-  var elm_thumbnail = document.querySelectorAll('.itemThumbnail > a > div')
-  if(elm_thumbnail.length >= 6){
-    
-      for(let i=0; i<elm_thumbnail.length;i++){
-        elm_thumbnail[i].style.background = `url(${elm_thumbnail[i].getAttribute('data-bg').replaceAll('=s72-', '=s260-').replaceAll(/-c$/g,'-a-k-no-nu').replaceAll(/-c\//g,'-a-k-no-nu/')})`;
-      }
+  if(document.querySelectorAll('.itemThumbnail > a > div')){
+    var elm_thumbnail = document.querySelectorAll('.itemThumbnail > a > div')
+    if(elm_thumbnail.length >= 6){
+        for(let i=0; i<elm_thumbnail.length;i++){
+          elm_thumbnail[i].style.background = `url(${elm_thumbnail[i].getAttribute('data-bg').replaceAll('=s72-', '=s260-').replaceAll(/-c$/g,'-a-k-no-nu').replaceAll(/-c\//g,'-a-k-no-nu/')})`;
+        }
     }
   }
 }
