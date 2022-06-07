0fd958a11e6f80fb86d0bd262e93d3f9b81acc98
Cuongbok <92067448+cuongbokreal@users.noreply.github.com>
2022-06-07 12:59:00 +0700

    Update main.js

--- cuongbokit/extension/fix-blur-thumbnail/main.js
+++ cuongbokit/extension/fix-blur-thumbnail/main.js
@@ -1,20 +1,26 @@
-var elm_thumbnail = document.querySelectorAll('.itemThumbnail > a > div')
-
-function replaceThumbnail(){
-  for(let i=0; i<elm_thumbnail.length;i++){
-  	if(elm_thumbnail[i].getAttribute('data-bg').match(/blogger\.googleusercontent\.com/g)){
-      elm_thumbnail[i].style.background = `url(${elm_thumbnail[i].getAttribute('data-bg').replaceAll('=s72-', '=s260-').replaceAll('-c','-a-k-no-nu')})`;
+if(document.querySelectorAll('.itemThumbnail > a > div')){
+  var elm_thumbnail = document.querySelectorAll('.itemThumbnail > a > div')
+  if(elm_thumbnail.length >= 6){
+    function replaceThumbnail(){
+      for(let i=0; i<elm_thumbnail.length;i++){
+        elm_thumbnail[i].style.background = `url(${elm_thumbnail[i].getAttribute('data-bg').replaceAll('=s72-', '=s260-').replaceAll('-c','-a-k-no-nu')})`;
       }
+    };
+    setTimeout(replaceThumbnail,3000)
   }
 }
-if(elm_thumbnail.length >= 6){
-setTimeout(replaceThumbnail,3000)
-}
+
 
 /*
 
 var elm_thumbnail = document.querySelectorAll('.itemThumbnail > a > div')
 for(let i=0; i<elm_thumbnail.length;i++){
       elm_thumbnail[i].style.background = `url(${elm_thumbnail[i].getAttribute('data-bg').replaceAll('=s72-', '=s260-').replaceAll('-c','-a-k-no-nu')})`;
-  }
+}
+  
+  
+  
+ if(elm_thumbnail[i].getAttribute('data-bg').match(/blogger\.googleusercontent\.com/g)){
+          elm_thumbnail[i].style.background = `url(${elm_thumbnail[i].getAttribute('data-bg').replaceAll('=s72-', '=s260-').replaceAll('-c','-a-k-no-nu')})`;
+}
   */
