655cac9d31866d871b4031292019702a91e6b68e
Cuongbok <92067448+cuongbokreal@users.noreply.github.com>
2022-06-20 18:59:52 +0700

    Update custom.js

--- accesstrade/coupon-site/cuongbokit/v3/custom.js
+++ accesstrade/coupon-site/cuongbokit/v3/custom.js
@@ -2,9 +2,14 @@ var updateTime = new Date(); //updateTime
 var today = `${updateTime.getDate()}.${updateTime.getMonth() + 1}`;
 document.querySelectorAll('.postTitle')[0].innerHTML += today;
 
-//location.hash coupon main when click next
-const pageItem = document.querySelectorAll('.atEQPOIVFSDFSDG-pagination > .atEQPOIVFSDFSDG-page-item');
-pageItem.forEach((element) => {element.addEventListener("click", couponTab);});
+window.addEventListener('DOMContentLoaded', (event) => {
+	console.log('DOM fully loaded and parsed');
+	//location.hash coupon main when click next
+	const pageItem = document.querySelectorAll('.atEQPOIVFSDFSDG-pagination > .atEQPOIVFSDFSDG-page-item');
+	pageItem.forEach((element) => {element.addEventListener("click", couponTab);});
+});
+
+
 function couponTab() {
 	location.hash = "";
 	location.hash = "second-block"
