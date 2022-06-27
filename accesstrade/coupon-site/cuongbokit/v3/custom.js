6e1048cbffc95b3a34c3c635f502ac0060ca04fc
Cuongbok <92067448+cuongbokreal@users.noreply.github.com>
2022-06-27 18:53:28 +0700

    Update custom.js

--- accesstrade/coupon-site/cuongbokit/v3/custom.js
+++ accesstrade/coupon-site/cuongbokit/v3/custom.js
@@ -29,17 +29,22 @@ function replaceBrackets(c){
 
 window.addEventListener('DOMContentLoaded', (event) => {
 	console.log('DOM fully loaded and parsed');
+
+	var filterSort = document.querySelectorAll('.atEQPOIVFSDFSDG-filter-sort')
+	filterSort[1].style.display = 'none';
+	
+	setTimeout(function(){
 	var voucherLen = document.querySelectorAll('.atEQPOIVFSDFSDG-nav-item > .atEQPOIVFSDFSDG-nav-link > em');
 	var totalVoucher = parseInt(replaceBrackets(voucherLen[0].innerText)) + parseInt(replaceBrackets(voucherLen[1].innerText));
-	//document.querySelectorAll('.postTitle')[0].innerHTML += ` (<span style="color:red">${totalVoucher}</span>) mã`
-	
-	var filterSort = document.querySelectorAll('.atEQPOIVFSDFSDG-filter-sort')
-	filterSort[1].style.display = 'none'
+	//document.querySelectorAll('.postTitle')[0].innerHTML += ` (<span style="color:red">${totalVoucher}</span>) mã`;
+	document.querySelector('.atEQPOIVFSDFSDG-list-title > h4').innerHTML = `HIỆN CÓ <span style='color:red;margin: 0px;'>${totalVoucher.toLocaleString()}</span> MÃ <span style='color:red;margin: 0px;'>(${add0To1Length(updateTime.getHours())}:${add0To1Length(updateTime.getMinutes())} ${today})</span>:`;
+},3000)
 	
 	//location.hash coupon main when click next
 	const pageItem = document.querySelectorAll('.atEQPOIVFSDFSDG-pagination > .atEQPOIVFSDFSDG-page-item');
 	pageItem.forEach((element) => {element.addEventListener("click", couponTab);});
 });
+
 
 document.querySelectorAll('[name="viewport"]')[0].setAttribute('content','width=device-width, initial-scale=1, user-scalable=1, minimum-scale=1, maximum-scale=1');
 
@@ -47,9 +52,12 @@ function couponTab() {
 	location.hash = "";
 	location.hash = "first-block";
 }
-
 function replaceBrackets(c){
 	return c.replaceAll(/\(|\)/g,'');
+}
+function add0To1Length(c){
+	if(c < 10){c = parseInt(`0${c}`)}
+	return c
 }
 /*
 document.querySelectorAll('.atEQPOIVFSDFSDG-voucher-main')[0].addEventListener('DOMContentLoaded', (event) => {
