6d2d5b27ce81ba8143e01672c2380246d6c89caf
Cuongbok <92067448+cuongbokreal@users.noreply.github.com>
2022-07-08 12:05:19 +0700

    Update get-fb-video.js

--- cuongbokit/facebook/get-fb-video.js
+++ cuongbokit/facebook/get-fb-video.js
@@ -56,11 +56,12 @@ var get_og_url = 1;
 			let a = JSON.parse(b.split("\n")[0]),
 				c = a.data.video.playable_url_quality_hd || a.data.video.playable_url;
 				var in4_video = ``;
+				if(get_owner_url === 1 || get_og_url ===1 || get_thumbnail ===1){in4_video = '%cThông tin video: \n%c'}
 				if(open_new_tab === 1){window.open(c)}
 				if(get_owner_url === 1){in4_video += `- Tác giả: https://facebook.com/${a.data.video.owner.id} \n`}
 				if(get_og_url === 1){in4_video += `- Link gốc: ${a.data.video.url} \n`}
 				if(get_thumbnail === 1){in4_video += `- Thumbnail: ${a.data.video.preferred_thumbnail.image.uri} \n`}
-			console.clear(), console.log("\u2705 Đã lấy thành công link download."), console.log("======================="), console.log("%cNhấp vào link này để tải xuống: " + c, "color: green"), console.log("Nếu trình duyệt của bạn không tự động tải xuống, vui lòng nhấn Ctrl + S trên bàn phím sau khi nhấp vào liên kết ở trên."), console.log('%c' + in4_video, "color:#ff9494"), console.log("======================="), console.log("\u{1F37A} Được tạo bởi https://fb.me/monokaijssss \n Chỉnh sửa, thêm chức năng bởi CuongbokIT: https://cuongbokit.blogspot.com \n -Link file gốc: https://gist.github.com/monokaijs/270e29620c46cabec1caca8c3746729d \n -Link file chỉnh sửa của CuongbokIT: https://github.com/cuongbokreal/api/blob/main/cuongbokit/facebook/get-fb-video.js \n -Link RAW: https://cuongbokreal.github.io/api/cuongbokit/facebook/get-fb-video.js")
+			console.clear(), console.log("\u2705 Đã lấy thành công link download."), console.log("======================="), console.log("%cNhấp vào link này để tải xuống: " + c, "color: green"), console.log("Nếu trình duyệt của bạn không tự động tải xuống, vui lòng nhấn Ctrl + S trên bàn phím sau khi nhấp vào liên kết ở trên."), console.log(in4_video, "color:#ff8484;font-family:system-ui;font-size:1.5rem;-webkit-text-stroke: 0.5px black;font-weight:bold", "color:#ff9494"), console.log("======================="), console.log("\u{1F37A} Được tạo bởi https://fb.me/monokaijssss \n Chỉnh sửa, thêm chức năng bởi CuongbokIT: https://cuongbokit.blogspot.com \n -Link file gốc: https://gist.github.com/monokaijs/270e29620c46cabec1caca8c3746729d \n -Link file chỉnh sửa của CuongbokIT: https://github.com/cuongbokreal/api/blob/main/cuongbokit/facebook/get-fb-video.js \n -Link RAW: https://cuongbokreal.github.io/api/cuongbokit/facebook/get-fb-video.js")
 		} catch (d) {
 			console.log("\u26A0\uFE0FKhông thể trích xuất dữ liệu. Có thể script này không còn hiệu quả nữa.")
 		}
