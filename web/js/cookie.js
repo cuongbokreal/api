00048ba1433634826995bc9932de3e9f1c0e6c2c
Cuongbok <92067448+cuongbokreal@users.noreply.github.com>
2022-07-16 21:43:58 +0700

    Update cookie.js

--- web/js/cookie.js
+++ web/js/cookie.js
@@ -20,3 +20,4 @@ function getCookie(name) {
 function eraseCookie(name) {   
     document.cookie = name +'=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
 }
+console.log('CuongbokIT Cookie import successful!')
