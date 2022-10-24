6aa2b428414fe5c986689a2d6e160728d429198d
Cuongbok <92067448+cuongbokreal@users.noreply.github.com>
2022-10-24 18:27:26 +0700

    Update saleTo.js

--- cuongbokit/ma-giam-gia/saleTo.js
+++ cuongbokit/ma-giam-gia/saleTo.js
@@ -1,1 +1,10 @@
+var content_saleTo = ``
+
+var updateTime = new Date(); //updateTime
+var mgg_saleTo = document.querySelector('#mgg_saleTo')
+thisDate = updateTime.getDate()
+if (thisDate == 15 || thisDate == 25 || thisDate = (updateTime.getMonth() + 1)){
+  mgg_saleTo.style.display = 'block';
+  mgg_saleTo.innerHTML = content_saleTo.replacaAll('\n', '<br/>')
+}
 
