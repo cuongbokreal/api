a52c26fcb707279c1e25117c0d11416d0da9d917
Cuongbok <92067448+cuongbokreal@users.noreply.github.com>
2022-10-25 19:38:47 +0700

    Update saleTo.js

--- cuongbokit/ma-giam-gia/saleTo.js
+++ cuongbokit/ma-giam-gia/saleTo.js
@@ -4,75 +4,14 @@ Full mã SHOPEE + LAZADA sale  𝟐𝟓.𝟏𝟎 (xem mã khung giờ ở phía 
 
 <b>A.SHOPEE</b>
 ➖➖➖➖SHOPEE➖➖➖➖
-🚛MIỄN PHÍ VẬN CHUYỂN: dùng tối đa 14 lượt
-• 18H: Lưu 4 mã Freeship giảm 15K/0Đ, 30K tại: https://shorten.asia/JwP5BuJD  (back)
-• 18H: Lưu mã Freeship giảm 70K/25K tại các live bất kì: https://shorten.asia/6R9nAqNw  (back)
-• Lưu 2 mã Freeship giảm 25K/50K lọc sp tại: https://shorten.asia/Q2HgtTSf  
-• Tặng sẵn 1 số mã Freeship trong ví tùy tài khoản: https://shorten.asia/sHB2gWeN 
-• Đổi mã Freeship giảm 50% giảm tối đa 40K tại: https://shorten.asia/ZMyqEnzW 
-• Quay mã Freeship 0Đ tại: https://shorten.asia/b58Z2njY 
+#SHOPEE 20H:
 .
-🔥MÃ ÁP SẢN PHẨM CÓ MÁC “Hoàn Xu Xtra” - NHẬP TAY:
-• 17H57-18H03: 2510HUNT250K hoàn 50% tối đa 250K đơn từ 250K. List sản phẩm áp mã: https://shorten.asia/VKGeX32a  (có thể back)
+⭐️ 1. Săn voucher cùng Shopee KOL - Mã áp toàn sàn Shopee: https://shorten.asia/DwnytsuH  
+► 20H: Lưu trên banner + dùng ngay mã giảm 30K cho đơn từ 150K toàn sàn Shopee (cho tất cả user)
+► 20H: Lưu trên banner + dùng ngay mã giảm 20K cho đơn từ 99K toàn sàn Shopee (cho tất cả user)
 .
-🔥SIÊU SALE VOUCHER - MÃ TOÀN SÀN: đã lưu sớm được
-• 18H: Mã giảm 25K đơn từ 75K
-=> Lưu mã tại: https://shorten.asia/AaRvQAqJ 
-.
-🔥KOL AFFILIATE - MÃ TOÀN SÀN: 18H là mã mới còn 19H, 20H là mã back
-• 18H: Mã giảm 30K đơn từ 99K
-• 18H, 19H, 20H: Mã giảm 10K đơn từ 50K
-• 18H, 19H, 20H: Mã giảm 15% tối đa 20K đơn từ 99K
-• 18H, 19H, 20H: Mã giảm 15% tối đa 30K đơn từ 150K
-=> Lưu mã tại: https://shorten.asia/DwnytsuH 
-.
-🔥MÃ LƯU TRÊN LIVE - MÃ ÁP ĐA SỐ SẢN PHẨM: mã back
-• 18H, 20H: Mã giảm 15K/50K, 20K/100K, 40K/250K. Lưu mã tại: https://shorten.asia/gdtFunaq  mục Live, gần giờ săn shop sẽ lên live
-• 18H, 20H: Mã giảm 30K/100K, 50K/250K. Lưu mã tại: https://shorten.asia/6R9nAqNw  mục “SĂN DEAL HOT”
-=> Tổng hợp list shop áp được mã 20K/100K, 40K/250K, 30K/100K, 50K/250K tại: https://shorten.asia/6R9nAqNw 
-.
-🔥MÃ ÁP SẢN PHẨM CÓ MÁC “Hoàn Xu Xtra” - LƯU BANNER: 
-• 18H: Mã hoàn 6% tối đa 50K đơn từ 100K(back)
-• 18H: Mã hoàn 6% tối đa 200K đơn từ 400K(back)
-=> Lưu mã tại: https://shorten.asia/6SyKtuPM 
-.
-🔥SHOPEEPAY: https://shorten.asia/4gnc1jC9 
-• 18H: SPPOCT25RH giảm 25% tối đa 15K đơn từ 50K(back)
-• Lưu thêm 2 mã giảm 10K tại: https://shorten.asia/4gnc1jC9 
-.
-🔥HÀNG HIỆU GIÁ TỐT: https://shorten.asia/TV5UVEg2 
-• 18H: 2510MDDHOAN70K0 hoặc 2510MDDHOAN70K1 hoặc 2510MDDHOAN70K2 hoàn 6% tối đa 70K đơn từ 600K. List sản phẩm áp mã: https://shorten.asia/HQsXPjBy  (back)
-• 18H: BATREND30K2510 giảm 8% tối đa 30K đơn từ 200K (back)
-.
-🔥THỜI TRANG: https://shorten.asia/FZ1S5YWh 
-• 18H: Săn deal 1K-9K tại banner: https://shorten.asia/FZ1S5YWh 
-• 18H: SUNNY25D (mã shop) giảm 220K đơn từ 600K cho Sunnyvali: https://shorten.asia/MkpbasTj 
-• 18H: QMJO220K (mã shop) giảm 220K đơn từ 600K cho QMJ Official Store: https://shorten.asia/rFKEvtUn 
-.
-🔥TIÊU DÙNG: https://shorten.asia/BBG7KNM5
-• 18H: Săn deal đồng giá 1K, 9K tại banner: https://shorten.asia/BBG7KNM5 
-.
-🔥NẠP THẺ - DỊCH VỤ: https://shorten.asia/fwKyvZk7 
-• 18H: Lưu mã giảm 25% tối đa 50K đơn từ 0Đ cho vé máy bay tại banner: https://shorten.asia/fwKyvZk7  (back)
-.
-🔥SHOP XU HƯỚNG: https://shorten.asia/rmwG7J1z 
-• 18H: Săn mã shop giảm 50K đơn từ 0Đ tại banner: https://shorten.asia/rmwG7J1z 
-• 18H: Săn deal cực sốc giá 1K tại banner: https://shorten.asia/rmwG7J1z 
-.
-🔥MÃ ÁP MỘT SỐ SẢN PHẨM CÒN LƯỢT:
-• AFF10TZW giảm 25K đơn từ 69K. List sản phẩm áp mã: https://shorten.asia/BwNVyVDw 
-• SKAMLTB2410 giảm 20K đơn từ 0Đ. List sản phấm áp mã: https://shorten.asia/Kh6JMER4 
-• SKAMLTS2410 giảm 15K đơn từ 0Đ. List sản phấm áp mã: https://shorten.asia/GaUwUKPN 
-• SKAMLTB1110 giảm 20K đơn từ 0Đ. List sản phẩm áp mã: https://shorten.asia/rCadMpzw 
-• SKAMLTS1110 giảm 15K đơn từ 0Đ. List sản phẩm áp mã: https://shorten.asia/g5m87Rfh 
-• LIFEMC10G giảm 50K đơn từ 350K. List sản phẩm áp mã: https://shorten.asia/whpYFKq6 
-.
-🔥VÀO ĐâY: https://shorten.asia/sHB2gWeN  NHẬP:
-• SX18x thay x bằng các số từ 210 đến 286 để tạo thành mã shop giảm 50K đơn từ 0Đ dùng vào 18H
-.
-🔥TỔNG HỢP CÁC BANNER LƯU MÃ KOL: https://shorten.asia/sHB2gWeN 
-.
-🔥TỔNG HỢP MÃ SHOP 18H: https://shorten.asia/sHB2gWeN
+⭐️ 2. Hirucars: https://shorten.asia/yKfdhnzk  
+► 20H: `HIRU20H` (mã shop) giảm 79k đơn từ 199k (Lỗi list sản phẩm áp mã)
 
 
 <b>B.LAZADA</b>
