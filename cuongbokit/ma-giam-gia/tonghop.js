020e4bfc3f476bedd9aec5c4b64c85b188e89065
Cuongbok <92067448+cuongbokreal@users.noreply.github.com>
2022-10-26 23:01:26 +0700

    Update tonghop.js

--- cuongbokit/ma-giam-gia/tonghop.js
+++ cuongbokit/ma-giam-gia/tonghop.js
@@ -1,56 +1,24 @@
 var api_mgg_content = `
 
 ➖➖➖➖SHOPEE➖➖➖➖
-🚛MIỄN PHÍ VẬN CHUYỂN:
-• 18H: Lưu mã Freeship giảm 15K/0Đ, 25K/100K tại: https://shorten.asia/QvQH5Z4f  (back)
-• 21H: Lưu mã Freeship giảm 10K/0Đ tại: https://shorten.asia/QvQH5Z4f  (mã mới)
-• Lưu mã Freeship giảm 25K/25K tại các live: https://shorten.asia/XHuzjW95  
-• Lưu mã Freeship giảm 25K/50K lọc sp: https://shorten.asia/9Jy3Aj9q  
-• Đổi mã Freeship giảm 50% giảm tối đa 40K: https://shorten.asia/cS9QPhqW 
-• Quay mã Freeship 0Đ: https://shorten.asia/TkwS4RA2 
+❤️ CÁC SỰ KIỆN GAME SHOPEE NGÀY 27/10 ❤️
 .
-🔥MÃ ÁP SẢN PHẨM CÓ MÁC “Freeship Xtra”: 
-• 17H57-18H03: 2610BATNGOTHU4 giảm 20K đơn từ 99K. List sản phẩm áp mã: https://shorten.asia/Hp2p6c2c  (back)
-• 19H57-20H03: 2610BATNGOTHU4 giảm 20K đơn từ 99K. List sản phẩm áp mã: https://shorten.asia/Hp2p6c2c  (back)
+🍒 1. Khám phá thị trấn Halloween (25/10 – 2/11): https://shorten.asia/GGpphHRJ  
+📚 CẨM NANG THỊ TRẤN:
+► Từ 19/10 đến 31/10: Tuần Lễ Thời Trang – Shopee Thú Cưng
+► Từ 25/10 đến 02/11: Đại Náo Đập Kẹo nhận đến 100K xu – Shopee Đập Kẹo
+► Từ 26/10 đến 31/10: Săn Combo Tiệc đổi 11K xu – Quà Tặng Shopee 
+► Từ 30/10 đến 01/11: Đổi xu khủng 30K – Shopee Máy Gắp Thú
 .
-🔥MÃ SHOPEE LƯU TRÊN LIVE - MÃ ÁP SẢN PHẨM MUA TỪ GIỎ LIVE: mã back
-• 18H, 20H: Mã giảm 20K/100K, 40K/250K. Lưu mã tại: https://shorten.asia/zYxVhe2Y  mục “FREESHIP 25K”
-• 18H, 20H: Mã giảm 30K/100K, 50K/250K. Lưu mã tại: https://shorten.asia/zYxVhe2Y  mục “SĂN DEAL HOT”
-[ Tổng hợp list shop áp được mã 20K/100K, 40K/250K, 30K/100K, 50K/250K tại: https://shorten.asia/zYxVhe2Y  ]
+🍒 2. Thử Thách Shopee 0H: https://shorten.asia/57NXJeeJ           
 .
-🔥MÃ ÁP SẢN PHẨM CÓ MÁC “Hoàn Xu Xtra” - LƯU BANNER: 
-• 18H: Mã hoàn 6% tối đa 100K đơn từ 50K(back)
-• 18H: Mã hoàn 6% tối đa 200K đơn từ 400K(back)
-=> Lưu mã tại: https://shorten.asia/Ba8bEkGd 
+🍒 3. Hộp Quà Thần Kỳ – LỌC TK: https://shorten.asia/SrP3eZWH                
 .
-🔥Coolmate: https://shorten.asia/2FesC4g4 
-• 18H42s: COOLM2618 (mã shop) giảm 300K đơn từ 0Đ. List sản phẩm áp mã: https://shorten.asia/6zDTfZsY 
-• 22H23s: COOLM2622 (mã shop) giảm 300K đơn từ 0Đ. List sản phẩm áp mã: https://shorten.asia/XuCA8VXs 
+🍒 4. Shopee Máy Gắp Thú: https://shorten.asia/Ksvpy43q   
 .
-🔥HIGHCLUB: https://shorten.asia/ejPhWuxk 
-• 21H48s: HIGH2621 (mã shop) giảm 69K đơn từ 199K. List sản phẩm áp mã: https://shorten.asia/czDuhZPH 
+🍒 5. Quà Tặng Shopee: Săn Combo Tiệc đổi 11K xu: https://shorten.asia/J2KWKKRc  
 .
-🔥MÃ ÁP MỘT SỐ SẢN PHẨM CÒN LƯỢT:
-• SKAMLTS2610 giảm 15K đơn từ 0Đ. List sản phẩm áp mã: https://shorten.asia/EG9gJRZX 
-• SKAMLTB2610 giảm 20K đơn từ 0Đ. List sản phẩm áp mã: https://shorten.asia/DzJqMP1s 
-• SKAMLTB2410 giảm 20K đơn từ 0Đ. List sản phấm áp mã: https://shorten.asia/mvNZzQND 
-• SKAMLTB1110 giảm 20K đơn từ 0Đ. List sản phẩm áp mã: https://shorten.asia/axv9WCxT 
-• SKAMLTS1110 giảm 15K đơn từ 0Đ. List sản phẩm áp mã: https://shorten.asia/TsS2Jhz1 
-• AFFBIGWED giảm 15% tối đa 49K đơn từ 299K. List sản phẩm áp mã: https://shorten.asia/uFwJqf9n 
-• BATREND30KT4FSD4 giảm 8% tối đa 30K đơn từ 200K. List sản phẩm áp mã: https://shorten.asia/cn3VaTSV 
-• LIFEMC10G giảm 50K đơn từ 350K. List sản phẩm áp mã: https://shorten.asia/G3v1KEWP 
-.
-🔥MỘT SỐ MÃ SHOP CÒN LƯỢT: 
-• COOLP35 giảm 50% tối đa 100K đơn từ 0Đ cho shop Coolmom: https://shorten.asia/bg7bTuwh 
-• THEBXLILY giảm 100K đơn từ 500K cho shop The Bad Good: https://shorten.asia/Nucqk1cN 
-• LOVIVNAHB giảm 50% không giới hạn cho shop LOVITO: https://shorten.asia/jqJ17GZq 
-• LMCA2010, LMCA1910, LMCA19102 giảm 50% tối đa 100K đơn từ 0Đ cho shop LMcation: https://shorten.asia/73sVwRT2 
-• Lưu, dùng luôn các mã shop Aviano Menswear: https://shorten.asia/92JPKmAF 
-.
-🔥VÀO ĐâY: https://shorten.asia/hKAxUVyZ  NHẬP:
-• SMIC1x thay x bằng các số từ 3192 đến 3657 để tạo thành mã shop giảm 50% tối đa 50K đơn từ 0Đ
-• SMIC1x thay x bằng các số từ 1696 đến 2186 để tạo thành mã shop giảm 50% tối đa 50K đơn từ 0Đ
-=> Tổng hợp các mã shop giảm 50K/0Đ xịn tại: https://shorten.asia/hKAxUVyZ 
+🍒 6. Nông Trại Shopee: Ngày hội bạn bè: https://shorten.asia/cbsg7kSS 
 
 
 <b>LAZADA</b>
