0e4cb92a5681494f36b007c27bd0eaae36b69596
Cuongbok <92067448+cuongbokreal@users.noreply.github.com>
2022-11-10 17:44:41 +0700

    Update saleTo.js

--- cuongbokit/ma-giam-gia/saleTo.js
+++ cuongbokit/ma-giam-gia/saleTo.js
@@ -96,9 +96,10 @@ var mgg_dday = document.querySelector('#mgg_dday')
 thisDate = updateTime.getDate();
 thisMonth = (updateTime.getMonth() + 1)
 var dday = 0
-if(thisDate == thisMonth){dday = thisDate}else
-if(thisDate > 0 && thisDate <= 15){dday = 15}else
-if(thisDate > 15 && thisDate <= 25){dday = 25}
+if(thisDate == thisMonth){dday = thisDate}else //cùng ngày
+if(thisDate + 1 == thisMonth){dday = thisMonth}else //trước 1 ngày
+if(thisDate > 0 && thisDate <= 15){dday = 15}else //15 hàng tháng
+if(thisDate > 15 && thisDate <= 25){dday = 25} //25 hàng tháng
 
 if (content_dday.length >= 20){
   content_dday = `<h4  class="title-block">🔥 Tiết Lộ Mã Giảm Giá ngày SALE <span style="color:red">${dday}.${thisMonth}</span></h4>` + content_dday
