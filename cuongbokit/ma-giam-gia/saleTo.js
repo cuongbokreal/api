5f1121139c526d33c952949daa6323032d4bfc4b
Cuongbok <92067448+cuongbokreal@users.noreply.github.com>
2022-11-10 17:41:33 +0700

    Update saleTo.js

--- cuongbokit/ma-giam-gia/saleTo.js
+++ cuongbokit/ma-giam-gia/saleTo.js
@@ -96,7 +96,7 @@ var mgg_dday = document.querySelector('#mgg_dday')
 thisDate = updateTime.getDate();
 thisMonth = (updateTime.getMonth() + 1)
 var dday = 0
-if(thisDate == thisMonth){dday = thisDate}
+if(thisDate == thisMonth){dday = thisDate}else
 if(thisDate > 0 && thisDate <= 15){dday = 15}else
 if(thisDate > 15 && thisDate <= 25){dday = 25}
 
