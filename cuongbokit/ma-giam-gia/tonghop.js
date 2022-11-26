ee992699beaff29f4b161472501b2304a332aab1
Cuongbok <92067448+cuongbokreal@users.noreply.github.com>
2022-11-26 11:45:01 +0700

    Update tonghop.js

--- cuongbokit/ma-giam-gia/tonghop.js
+++ cuongbokit/ma-giam-gia/tonghop.js
@@ -16,62 +16,85 @@ https://shorten.asia/g9JHz9Th
 🎁 Bí ẩn Hộp Quà Đen chia kho 50 triệu xu 
 https://shorten.asia/wySec8tA 
 
+➖➖➖➖SHOPEE➖➖➖➖
+🚛MIỄN PHÍ VẬN CHUYỂN:
+• 9H: Lưu mã Freeship Xtra, 10K/0Đ, 25K/50K tại: https://tinyurl.com/2jzmu4ds  (back)
+• 9H: Lưu mã Freeship giảm 25K/0Đ 15K/0Đ tại các live: https://tinyurl.com/23y2e549  (back)
+• Đổi mã Freeship giảm 50% giảm tối đa 40K: https://tinyurl.com/27n4wdmy 
+• Quay mã Freeship: https://tinyurl.com/29b6ffex 
+.
+🔥MÃ TOÀN SÀN: 
+• 9H: 2611HIAM10K0PH hoặc 2611HIAM10K1PH hoặc 2611HIAM10K2PH giảm 10K đơn từ 150K(back)
+• 9H: Mã giảm 5% tối đa 50K đơn từ 0Đ. Lưu mã tại: https://tinyurl.com/27n4wdmy 
+.
+🔥MÃ ÁP SẢN PHẨM CÓ MÁC “Hoàn Xu Xtra”: 
+• 9H: 263011HOAN40K0PH hoặc 263011HOAN40K1PH hoặc 263011HOAN40K2PH hoàn 5% tối đa 40K đơn từ 500K(back)
+• 9H: 263011MALL50K0PH hoặc 263011MALL50K1PH hoặc 263011MALL50K2PH hoàn 5% tối đa 50K đơn từ 250K ShopeeMall(back)
+.
+🔥MÃ ÁP MỘT SỐ SẢN PHẨM: 
+• 9H: SRNOVFS giảm 50% tối đa 20K đơn từ 0Đ. List sản phẩm áp mã: https://tinyurl.com/2925qjtc  (có thể back)
+• 9H: SRNOVP320 giảm 20K đơn từ 50K. List sản phẩm áp mã: https://tinyurl.com/2q37abpk  (có thể back)
+• 9H: SRNOVP315 giảm 50% tối đa 15K đơn từ 0Đ. List sản phẩm áp mã: https://tinyurl.com/2qauvm4b  (có thể back)
+.
+🔥SHOPBALOTUI: https://tinyurl.com/2j7lknwc 
+• 9H6s: HOAN2611B (mã shop) giảm 50K đơn từ 0Đ. List sản phẩm áp mã: https://tinyurl.com/2oq4hs6t 
+.
+🔥XEM LIVE SĂN MÃ SHOPEE GIẢM 25K/50K, 50K/100K + FREESHIP 70K/25K TỪ THƯƠNG HIỆU: mục live
+• 11H: Thương hiệu LIXIBOX: https://tinyurl.com/27vndwcg 
+• 11H: Thương hiệu FOCALLURE: https://tinyurl.com/2fy64ulp 
+.
+🔥MÃ ÁP ĐA SỐ SẢN PHẨM: 
+• AFFCSM79 giảm 18K đơn từ 99K. List sản phẩm áp mã: https://tinyurl.com/2nfrmuju 
+.
+🔥VÀO ĐâY: https://tinyurl.com/2yfqztjq  NHẬP:
+• TRIVx thay x bằng các số từ 10751 đến 12973 để tạo thành mã shop giảm 50% tối đa 20K/0Đ, 10K/0Đ, 50K/50K
+• TRIVx thay x bằng các số từ 1 đến 8539 để tạo thành mã shop giảm 50% tối đa 20K/0Đ, 10K/0Đ, 50K/50K
+.
+🔥ĐÓN CHỜ SIÊU SALE 12/12: 
+• Mã hoàn xu đến 1,2tr + ShopeePay giảm 1,2tr + điện tử hoàn 1tr + thời trang, quốc tế hoàn 25K/150K + mã khác…
+=> Lưu sớm mã tại: https://tinyurl.com/2lankfdw  (kéo xuống dưới)
+.
+🔥THỬ THÁCH SHOPEE: https://tinyurl.com/2cryw9nu 
+
 <b>LAZADA</b>
-⏰MÃ LAZADA NGÀY 26/11:
-🔥0H: MÃ TOÀN SÀN GIẢM 25K ĐƠN TỪ 99K: đúng giờ load trang lưu mã
-=> Lưu mã tại(1): https://shorten.asia/qzR3hyhW 
-=> Lưu mã tại(2): https://shorten.asia/BnzvefFW 
-=> Lưu mã tại(3): https://shorten.asia/S1hgsUXz 
-=> Lưu mã tại(4): https://shorten.asia/RgTKTt6H 
-=> Lưu mã tại(5): https://shorten.asia/2brdbHFy 
+➖➖➖➖LAZADA➖➖➖➖
+🔥9H: MÃ TOÀN SÀN GIẢM 500K/0Đ, 150K/1500K, 50K/600K, 30K/400K, 6%: canh back - có thể lên sớm hoặc muộn hơn
+=> Lưu mã tại: https://tinyurl.com/29oh9go3 
 .
-🔥0H: TOÀN SÀN GIẢM 500K/0Đ, 150K/1500K, 50K/600K, 30K/400K, 6%: back vào 9H, 12H, 15H, 20H
-=> Lưu mã tại: https://shorten.asia/NtQePTCX  (0h lên mã mới)
+🔥9H: MÃ HOÀN TIỀN MAX 12% TỐI ĐA 300K ĐƠN TỪ 399K: canh back - có thể lên sớm hoặc muộn hơn
+=> Lưu mã tại: https://tinyurl.com/2bxqx28n 
 .
-🔥0H: MÃ HOÀN TIỀN MAX 12% TỐI ĐA 300K ĐƠN TỪ 399K: back vào 9H, 12H, 15H, 20H
-=> Lưu mã tại: https://shorten.asia/wNzwJgQU  (0h lên mã mới)
+🔥9H: MÃ NGâN HÀNG: load trang liên tục - một số mã có thể lên sớm hoặc muộn hơn
+=> Lưu mã tại: https://tinyurl.com/2xmek8br 
 .
-🔥0H: MÃ LAZMALL GIẢM 50K ĐƠN TỪ 400K:
-=> Lưu mã tại: https://shorten.asia/a9pC6R1m 
+🔥9H: MÃ ZALOPAY + MOMO + VNPT: canh back - có thể lên sớm hoặc muộn hơn
+=> Lưu mã tại: https://tinyurl.com/2pphvs7y 
 .
-🔥0H, 9H, 12H, 20H: DEAL TRỢ GIÁ - “LazFlash Crazy”:
-=> Xem chi tiết tại: https://shorten.asia/aefeVfCZ 
-.
-🔥9H, 12H: MÃ ZALOPAY + MOMO + VNPT: canh back - có thể lên sớm hoặc muộn hơn
-=> Lưu mã tại: https://shorten.asia/PpbcuHWm 
-.
-🔥9H, 12H: MÃ NGâN HÀNG: load trang liên tục - một số mã có thể lên sớm hoặc muộn hơn
-=> Lưu mã tại: https://shorten.asia/akA2xweR 
+🔥9H: DEAL TRỢ GIÁ - “LazFlash Crazy”:
+=> Xem chi tiết tại: https://tinyurl.com/2cpqf3sp 
 .
 🔥MỘT SỐ BANNER KOL CÓ THỂ LÊN MÃ 10K/50K, 25K/99K, 50K/399K BẤT NGỜ: 
-• HL: https://shorten.asia/DPvK66WT 
-• Mai: https://shorten.asia/fR5eJzW6 
-• CHL: https://shorten.asia/ETy5sWjv 
-• Vina: https://shorten.asia/RbDtwsBr 
-• Mobi: https://shorten.asia/vMKhDP4f 
-• OLP: https://shorten.asia/PEtbbXXN 
-• Shop:  https://shorten.asia/2brdbHFy 
+• Vinaphone: https://tinyurl.com/29c8nj8n 
+• Mobifone: https://tinyurl.com/22eb79yq 
+• New: https://tinyurl.com/25ua8qsr 
+• Shopiness: https://tinyurl.com/23j4qmaz 
+• Hà Linh: https://tinyurl.com/2yzkc9xa 
+• Mailovesbeauty: https://tinyurl.com/23tp288r 
+• Chloe: https://tinyurl.com/2c46pazr 
+• Badinulo: https://tinyurl.com/2f86cxn6 
+• Hà Trúc: https://tinyurl.com/27xxtah7 
 .
-🔥MỘT SỐ BANNER SALE KHÁC: 
-• Black Friday Sale: https://shorten.asia/GAx78mhJ 
-• Xả Hàng: https://shorten.asia/JdZa329S 
-• LazMall: https://shorten.asia/jZd3q6bf 
-• Top Bán Chạy: https://shorten.asia/eC3ChjRk 
-• Mua là Freeship: https://shorten.asia/gbRCDAXF 
-• Hoàn Tiền Gấp Đôi: https://shorten.asia/CBW63263 
-• Sale Thương Hiệu: https://shorten.asia/a9pC6R1m 
+🔥Lưu mã quốc tế giảm 10K/99K, 8%: https://tinyurl.com/2m5mnfrk  mục Laz Quốc Tế
 .
-🔥LAKOCSALE11 giảm 50K đơn từ 249K cho TEELAB: https://shorten.asia/W8jTJ2KD 
+🔥LAKOCSALE11 giảm 50K đơn từ 249K cho TEELAB: https://tinyurl.com/2dr5by94 
 .
-🔥YODYLAZADA giảm 50K đơn từ 300K cho Yody: https://shorten.asia/6WePH1S5 
+🔥YODYLAZADA giảm 50K đơn từ 300K cho Yody: https://tinyurl.com/2zvuhv34 
 .
-🔥LAZHOT50K giảm 50K đơn từ 200K cho 1 số sản phẩm: https://shorten.asia/H2u6RUjC 
+🔥LAZHOT50K giảm 50K đơn từ 200K cho 1 số sản phẩm: https://tinyurl.com/2oz8opfk 
 .
-🔥Sản phẩm dùng thử giá 9K, x9K: https://shorten.asia/mxHZgeMx 
+🔥Lưu mã shop CP giảm 50K đơn từ 299K: https://tinyurl.com/2m767f3a 
 .
-🔥0H: Lên mã đổi xu: https://shorten.asia/JGksXPN1 
-.
-🔥0H: Lên mã đổi sao: https://shorten.asia/Yt7d5kZJ 
+🔥Sản phẩm dùng thử giá 9K, x9K: https://tinyurl.com/23wzv4th 
 
 
 <b>TIKI</b>
