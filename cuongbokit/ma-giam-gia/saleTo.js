70e7a0771799cb72b803e1437ee7e6e0a8f1ad7b
Cuongbok <92067448+cuongbokreal@users.noreply.github.com>
2022-12-11 22:32:10 +0700

    Update saleTo.js

--- cuongbokit/ma-giam-gia/saleTo.js
+++ cuongbokit/ma-giam-gia/saleTo.js
@@ -1,6 +1,6 @@
 var content_dday = `
-
-BẬT MÍ SỚM 95% MÃ SHOPEE NGÀY SIÊU SALE 12/12:
+Tổng hợp MÃ SHOPEE + LAZADA NGÀY SIÊU SALE 12/12:
+<h2><b>LAZADA</b></h2>
 🚛MIỄN PHÍ VẬN CHUYỂN: dùng tối đa 24 lượt
 • 0H: Lưu 6 mã Freeship 0Đ, 30K/99K tại: https://shope.ee/99pGJWZQIq (còn back vào 9H, 12H, 15H, 18H, 21H)
 • 0H: Lưu mã Freeship giảm 99K/25K, 25K tại các live bất kì: https://shope.ee/5zsEXniMq1 (còn back vào 9H, 12H, 15H, 18H, 20H)
@@ -276,7 +276,128 @@ BẬT MÍ SỚM 95% MÃ SHOPEE NGÀY SIÊU SALE 12/12:
 📌Beauty Club: https://shope.ee/2prqhjcDbM
 • Voucher, ưu đãi từ các shop bán
 .
-ĐẦY ĐỦ MÃ NGÀY SIÊU SALE 12/12 TỐI NAY MÌNH CẬP NHẬT SAU
+
+<h2><b>LAZADA</b></h2>
+⏰TỔNG HỢP MÃ LAZADA NGÀY SIÊU SALE 12/12:
+🔥MÃ TOÀN SÀN - LƯU BANNER: 
+• 0H: Mã giảm 30K đơn từ 99K. Lưu mã tại: https://tinyurl.com/2psyhpzk 
+• 0H, 19H: Mã giảm 30K đơn từ 99K. Lưu mã tại: https://tinyurl.com/2j4dhukv 
+• 0H: Mã giảm 30K đơn từ 112K. Lưu mã tại: https://tinyurl.com/2eu3aczf 
+• 0H: Mã giảm 30K đơn từ 112K. Lưu mã tại: https://tinyurl.com/2le9zgvp 
+• 12H: Mã giảm 30K đơn từ 112K. Lưu mã tại: https://tinyurl.com/2fyo4xk6 
+• 0H: Mã giảm 25K đơn từ 150K. Lưu mã tại: https://tinyurl.com/2k4wqv5g 
+• 0H, 12H: Mã giảm 10K đơn từ 50K. Lưu mã tại: https://tinyurl.com/2qxz625d 
+=> Load trang liên tục từ phút 58 đến 05 nhé (mã có thể lên sớm hoặc muộn hơn vài phút)
+.
+🔥MÃ TOÀN SÀN - VOUCHER SĂN TRƯỚC GIỜ G:
+• 21H ngày 11/12: Mã giảm 20% tối đa 50K 
+• 21H30 ngày 11/12: Mã giảm 25% tối đa 100K 
+• 22H ngày 11/12: Mã giảm 30% tối đa 200K 
+• 22H30 ngày 11/12: Mã giảm 35% tối đa 300K 
+• 23H ngày 11/12: Mã giảm 40% tối đa 450K 
+• 23H30 ngày 11/12: Mã giảm 45% tối đa 500K 
+• 0H ngày 12/12: Mã giảm 50% tối đa 1000K + Mã giảm 100K/1500K
+=> Lưu mã tại: https://tinyurl.com/2gbzsszf 
+.
+🔥0H: MÃ GIẢM 50K/1TR, 100K/2TR5,...+ CÓ THỂ LÊN THÊM MÃ BẤT NGỜ KHÁC: back vào 9H, 12H, 15H, 18H, 20H
+=> Lưu mã tại: https://tinyurl.com/2gbzsszf 
+.
+🔥0H: MÃ QUỐC TẾ GIẢM 121K ĐƠN TỪ 300K: 
+=> Lưu mã tại: https://tinyurl.com/2n8howuw 
+.
+🔥0H: MÃ LAZMALL GIẢM 45K ĐƠN TỪ 400K: 
+=> Lưu mã tại: https://tinyurl.com/2hmknxkw 
+.
+🔥0H: LƯU MÃ GIẢM 15% TỐI ĐA 100K ĐƠN TỪ 250K CHO SỨC KHỎE SẮC ĐẸP: 
+=> Lưu mã tại: https://tinyurl.com/2fy5nf92 
+.
+🔥0H: MÃ ZALOPAY GIẢM 50K/499K, 10% + MÃ MOMO GIẢM 50K/999K, 20K/399K, 10K, 5K + MÃ VNPT GIẢM 15K/59K: back vào 9H, 12H, 20H
+=> Lưu mã tại: https://tinyurl.com/2pzv7uxn 
+.
+🔥0H: MÃ NGâN HÀNG: back vào 9H, 12H, 20H
+=> Lưu mã tại: https://tinyurl.com/2e6rkg4f 
+.
+🔥0H: MÃ HOÀN TIỀN MAX 10% TỐI ĐA 500K ĐƠN TỪ 1TR + MÃ HOÀN TIỀN MAX 20K ĐƠN TỪ 0Đ: back vào 9H, 12H, 15H, 20H 
+=> Lưu mã tại: https://tinyurl.com/2gcrlfoj 
+.
+🔥19H: MÃ TIÊU DÙNG, BÁCH HÓA GIẢM 40K/212K + 21H: MÃ SỨC KHỎE, LÀM ĐẸP GIẢM 40K/212K:
+=> Lưu mã tại: https://tinyurl.com/2fyo4xk6 
+.
+🔥VOUCHER TÍCH LŨY GIẢM ĐẾN 800K: 
+• Thu thập hằng ngày. Dùng từ ngày 12-14/12. Giảm 15K cho mỗi 150K. Giảm 50K cho mỗi 650K
+=> Thu thập voucher tích lũy tại: https://tinyurl.com/2gbzsszf 
+=> Thu thập thêm voucher tích lũy dưới sản phẩm: https://tinyurl.com/2k6fcmrw 
+=> Thu thập thêm voucher tích lũy bằng cách làm nhiệm vụ tại: https://tinyurl.com/2exddox6 
+.
+
+🔥9H, 20H: LƯU MÃ GIẢM 20% TỪ CÁC THƯƠNG HIỆU: 
+• Hoàn tiền max 12%: https://tinyurl.com/2mjvwzpj 
+• Top bán chạy: https://tinyurl.com/2o7wgp8m 
+• Freeship toàn quốc: https://tinyurl.com/2n8db3hr 
+• Rẻ vô cực: https://tinyurl.com/2gxkf7jp 
+• Càng mua càng giảm: https://tinyurl.com/2zbpuc5y 
+• Tất cả deal: https://tinyurl.com/2pp4na2d 
+.
+🔥0H: LƯU MÃ SHOP GIẢM 30% + 12H: SĂN DEAL ĐỒNG GIÁ 12K TỪ CÁC THƯƠNG HIỆU THỜI TRANG: 
+• Thời trang nữ: https://tinyurl.com/2zgdxxf8 
+• Thời trang nam: https://tinyurl.com/2fgq6b4o 
+• Túi balo, phụ kiện: https://tinyurl.com/2kowjyzk 
+• Thời trang cho bé: https://tinyurl.com/2qukd5au 
+• Thể thao, dã ngoại: https://tinyurl.com/2z4ryye5 
+.
+🔥0H, 9H, 20H: LƯU MÃ SHOP GIẢM 20% TỪ CÁC THƯƠNG HIỆU SỨC KHỎE SẮC ĐẸP: 
+• Sức khỏe: https://tinyurl.com/2gyc777k 
+• Làm đẹp: https://tinyurl.com/2hz38tvs 
+.
+🔥0H, 9H, 20H: LƯU MÃ SHOP GIẢM 20% TỪ CÁC THƯƠNG HIỆU BÁCH HÓA MẸ BÉ: 
+• Bách hóa: https://tinyurl.com/2gc9vu4w 
+• Chăm sóc cá nhân: https://tinyurl.com/2mcbsvuw 
+• Mẹ bé: https://tinyurl.com/2por35hy 
+.
+🔥0H, 2H, 12H, 16H, 20H: LƯU MÃ SHOP GIẢM 20% TỪ CÁC THƯƠNG HIỆU NHÀ CỬA ĐỜI SỐNG: 
+• Ô tô, xe máy: https://tinyurl.com/2hqxwlbr 
+• Sở thích, học tập: https://tinyurl.com/2zwfgu9e 
+• Sửa chữa, sân vườn: https://tinyurl.com/2ecnfokq 
+• Điện gia dụng: https://tinyurl.com/2el9dp4g 
+• Trang trí nhà cửa: https://tinyurl.com/2omd6khw 
+• Nội thất, đồ đạc: https://tinyurl.com/2nr3gfzw 
+• Nấu ăn, dọn dẹp: https://tinyurl.com/2f4oa593 
+.
+🔥ƯU ĐÃI TỪ CÁC THƯƠNG HIỆU ĐIỆN TỬ: 
+• Thiết bị công nghệ: https://tinyurl.com/2joug6x7 
+• Công nghệ giải trí: https://tinyurl.com/2m5lrexd 
+• Di động phụ kiện: https://tinyurl.com/2exeh4nu 
+.
+🔥CÁC BANNER KOL CÓ THỂ LÊN MÃ TOÀN SÀN GIẢM 10K/50K, 25K/99K, 50K/399K BẤT NGỜ: 
+• New: https://tinyurl.com/2k4wqv5g  
+• Shopiness: https://tinyurl.com/2qxz625d 
+• Vinaphone: https://tinyurl.com/2gyrw7ul 
+• Mobifone: https://tinyurl.com/2g7sy4we 
+• Hà Linh: https://tinyurl.com/2z953nqz 
+• Mailovesbeauty: https://tinyurl.com/2o3s64lg 
+• Chloe: https://tinyurl.com/2j8h6vg5 
+• Badinulo: https://tinyurl.com/2os2g6tt 
+• Hà Trúc: https://tinyurl.com/2ek8lqzr 
+.
+🔥0H, 9H, 12H, 20H: DEAL TRỢ GIÁ - “LazFlash Crazy”:
+=> Xem chi tiết tại: https://tinyurl.com/2nlgfwsv 
+.
+🔥MÃ NHẬP TAY GIẢM 30K/30K, 30K/99K, 50K/350K, 120K: chờ cập nhật trên nhóm zalo, tele
+.
+🔥MINIGAME TẶNG SON ESTEE LAUDER TRỊ GIÁ 960K CHO 4 BẠN MAY MẮN:
+=> Xem chi tiết tại: https://t.ecomobi.com/ 
+.
+🔥Ưu đãi đặt cọc trước: https://tinyurl.com/2jmvnuva 
+.
+🔥Sản phẩm dùng thử 9K, X9K: https://tinyurl.com/2j5qrtuu 
+.
+🔥Tham gia game trúng giỏ hàng 1,2tr: https://tinyurl.com/2lvz9wym 
+.
+🔥Lưu sớm mã shop Adidas giảm 20%, 12%: https://tinyurl.com/2gqq327m 
+.
+🔥0H: Lên mã đổi xu: https://tinyurl.com/2e9bo5v8 
+.
+🔥0H: Lên mã đổi sao: https://tinyurl.com/2z6r43mm 
 
 `
 
