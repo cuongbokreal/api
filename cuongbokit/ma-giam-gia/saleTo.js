2091558e19c377451098e6f99a3acf38f456bfa8
Cuongbok <92067448+cuongbokreal@users.noreply.github.com>
2022-12-14 10:40:19 +0700

    Update saleTo.js

--- cuongbokit/ma-giam-gia/saleTo.js
+++ cuongbokit/ma-giam-gia/saleTo.js
@@ -1,4 +1,230 @@
 var content_dday = `
+Mã các khung giờ Hot 9H, 12H, 15H, 18H, 21H sẽ được tiết lộ trước 30 phút!
+.
+🚛 1. Chính sách miễn phí vận chuyển 15/12:
+► Săn mã Freeship 0Đ thêm lượt vào 6 khung giờ vàng: 0H – 9H – 12H – 15H – 18H – 21H gồm:
+– Mã Freeship tối đa 10K đơn từ 0Đ
+– Mã Freeship -50% tối đa 30K đơn từ 99K
+Tất cả mã freeship lưu tại đây nhé: https://tinyurl.com/2pdczqtr
+► Ngoài ra, Shopee tặng bạn thêm 8 mã miễn phí vận chuyển đã có sẵn trong ví voucher cho ngày 15/12 gồm:
+– 5 Mã Freeship đơn từ 0Đ
+– 3 mã freeship extra đến 70K
+► Số lượt mã freeship được dùng tối đa ngày 15/12 là: 24 lượt/ngày
+.
+🎁 2. Voucher Shopee độc quyền từ TrumGiamGia:
+► Sale 15/12 ad được Shopee tặng mã độc quyền 20K/99K, 30K/150K toàn sàn Shopee. Mã sẽ được tiết lộ trước 15 phút (vào lúc 23h45 ngày 14/12). Mn nhớ theo dõi để lấy mã nhé!
+.
+⭐️ 3. Voucher từ các KOL Shopee:
+► Nếu bạn muốn nhận tin thông báo nhanh nhất các voucher độc quyền giảm 20K/99K, 30k/99k, 30K/150K… toàn sàn Shopee đơn từ các KOL khác ngày 15/12 thì hãy theo dõi mã được đăng lên sát giờ 0H, 9H, 12H, 18H, 21H và cập nhật liên đơn từc trên group Zalo tại đây: https://zalo.me/g/fqehfa279
+.
+⭐️ 4. Tổng hợp list deal cực hot 15/12:
+► Deal Premium 0Đ: https://tinyurl.com/2kctrdrm
+► Hàng hiệu cao cấp Mua 1 tặng 1: https://tinyurl.com/2hva6fmh
+► Siêu Combo Mua 1 tặng 1: https://tinyurl.com/2ed92745
+► Deal đồng giá đơn từ 1K: https://tinyurl.com/2nkle8jd
+► Deal đồng giá đơn từ 9K: https://tinyurl.com/2e3rrgfg
+► Deal đồng giá đơn từ 59K: https://tinyurl.com/2go7eul6
+.
+⭐️ 5. Săn Voucher tại banner KOL Shopee: https://tinyurl.com/2hygwber
+► 0H30, 7H30, 11H30, 18H30: Lưu trên banner + dùng ngay mã giảm 50% tối đa 150K đơn từ 150k toàn sàn Shopee.
+👉 Lúc 0H, 6H, 9H, 12H, 18H, 21H vào banner lưu + dùng ngay các voucher bên dưới:
+► Giảm 15% tối đa 40K cho đơn từ 200K toàn sàn Shopee (áp dụng cho tất cả người dùng).
+► Giảm 15% tối đa 40K cho đơn từ 200K toàn sàn Shopee (áp dụng cho người dùng nhất định).
+► Giảm 15% tối đa 30K cho đơn từ 150K toàn sàn Shopee (áp dụng cho tất cả người dùng).
+► Giảm 15% tối đa 30K cho đơn từ 150K toàn sàn Shopee (áp dụng cho người dùng nhất định).
+👉 Lúc 0H30,1H, 6H, 7H, 8H, 9H, 10H, 11H, 12H, 13H, 14H, 15H, 16H, 17H, 18H, 19H, 20H vào banner lưu + dùng ngay các voucher bên dưới
+▶Giảm 15% tối đa 20K cho đơn 99K (áp dụng tất cả người dùng)
+▶Giảm 15% tối đa 30K cho đơn 150K (áp dụng người dùng nhất định)
+.
+⭐️ 6. Khung Giờ Săn Sale – Mã áp toàn sàn: https://tinyurl.com/2pdczqtr
+► 0H: 1512HUNT150K hoàn 15% tối đa 150K đơn từ 500K cho tất cả sản phẩm Hoàn Xu Xtra. List sản phẩm áp mã: https://tinyurl.com/2jphsx8l
+► 0h: 1512HUNT500K hoàn 50% tối đa 500K đơn từ 1 triệu cho tất cả sản phẩm Hoàn Xu Xtra. List sản phẩm áp mã: https://tinyurl.com/2p7jjat9
+► 0h: 1512EBVHOAN15K hoàn 15% tối đa 15K đơn từ 99K cho tất cả sản phẩm Hoàn Xu Xtra. List sản phẩm áp mã: https://tinyurl.com/2dvysxev
+► 0H: 1512EBVHOAN70K hoàn 8% tối đa 70K đơn từ 500K cho tất cả sản phẩm có logo Hoàn Xu Xtra. List sản phẩm áp mã: https://tinyurl.com/2g5q4bdb
+► 0H: 1512EBVMALL50K hoàn 8% tối đa 50K đơn từ 250K cho tất cả sản phẩm Shopee Mall có logo Hoàn Xu Xtra thuộc shop Mall. List sản phẩm áp mã: https://tinyurl.com/2nksgmkl
+.
+📌 6.1. Mã Áp Dụng Cho Nhiều Sản Phẩm:
+Các mã bên dưới sẽ back thêm lượt dùng lúc 9H, 12H, 15H:
+► AFF12PK1 giảm 25K đơn từ 125k. List sản phẩm áp mã: https://tinyurl.com/2pp5k6w5
+► AFF12PK2 giảm 15% tối đa 30K đơn từ 99k. List sản phẩm áp mã: https://tinyurl.com/2dr2wl8f
+.
+⭐️ 7. Trang Chủ Siêu Sale 15/12:
+📌 Các mã bên dưới áp dụng toàn sàn Shopee (mã đã được thêm sẵn trong ví voucher Shopee của bạn rồi). Bạn vào đây để kiểm tra xem mình có mã không nhé: https://tinyurl.com/2evqugt4
+► Mã giảm 5% tối đa 50K đơn từ 350K cho sản phẩm hoàn xu Xtra.
+► Mã hoàn 5% tối đa 200K đơn từ 500K cho sản phẩm hoàn xu Xtra.
+► Mã giảm 8K đơn từ 99K toàn sàn Shopee thanh toán qua ví ShopeePay.
+► Mã giảm 5% tối đa 400K đơn từ 300K cho sản phẩm thuộc ngành hàng điện đơn từ.
+► Mã giảm 5% tối đa 100K đơn từ 500K toàn sàn Shopee.
+► Mã giảm 5% tối đa 50K đơn từ 150K cho sản phẩm thuộc hàng Quốc Tế.
+► Mã giảm 5% tối đa 400K đơn từ 300K cho sản phẩm thuộc ngành hàng điện đơn từ.
+► Mã giảm 8K đơn từ 99K toàn sàn Shopee.
+► Mã giảm 8K đơn từ 99K cho sản phẩm thuộc ngành hàng thời trang
+.
+⭐️ 8. Các voucher bắt buộc phải lưu trước trên banner thì 12/12 mới dùng được:
+📌 8.1. Lưu voucher mở trước giờ G: https://tinyurl.com/2hygwber
+👉 Ngày 14.12 bạn vào link này https://tinyurl.com/2hygwber để lưu trước các mã bên dưới để dùng lúc 0H ngày 15.12 (nhớ đặt báo thức vào lưu mã đúng khung giờ nhé vì mã chỉ xuất hiện đúng 15 phút):
+► 21H – 21H15: Lưu voucher 30K toàn sàn shopee
+► 22H – 22H15: Lưu voucher 60K toàn sàn shopee
+► 23H – 23H15: Lưu voucher 90K toàn sàn shopee
+.
+📌 8.2. Lưu sớm Mã trên banner Tại Link Siêu Sale Voucher tháng 12: https://tinyurl.com/2mkolfa6
+► Lưu trước mã giảm 20K cho đơn từ 99K sử dụng lúc 23H30 ngày 15/12
+► Voucher giảm 150K cho đơn từ 1,5 triệu: Lưu trước trên banner vào 0h ngày 13/12 và 0H ngày 15/12. Sử dụng vào 0H, 9H ngày 15/12.
+► Voucher giảm 40K cho đơn từ 250K: Lưu trước trên banner vào 0H ngày 13/12 và 0H, 1H, 7H, 9H, 12H, 15H ngày 15/12. Sử dụng vào 0H, 1H, 7H, 9H, 12H, 15H, 18H ngày 15/12
+► Voucher giảm 10% tối đa 50K cho đơn từ 300K: Lưu trước trên banner vào 0H ngày 13/12 và 0H, 9H ngày 15/12. Sử dụng vào 0H, 9H, 12H ngày 15/12
+.
+📌 8.3. Lưu trước mã trên link Trở lại săn Deal: https://tinyurl.com/2kgxd2t6
+► 21h30: vào link lưu trước mã giảm 999K đơn từ 0đ toàn sàn Shopee (chỉ hiện cho 1 số user)?
+► 22h30: vào link lưu trước mã giảm 999K đơn từ 0đ toàn sàn Shopee (chỉ hiện cho 1 số user)?
+► 23h30: vào link lưu trước mã giảm 999K đơn từ 0đ toàn sàn Shopee (chỉ hiện cho 1 số user)?
+.
+📌 8.4. Lưu Trước Mã Giảm Cho Các Thương Hiệu Tại Link Siêu Sale Voucher: https://tinyurl.com/2ew82bvk
+.
+⭐️ 9. Shopee Hoàn Xu Xtra: https://tinyurl.com/2oqb6ht5
+► Lưu trước trên banner tại link này https://tinyurl.com/2p9kpsoj + Dùng lúc 0H ngày 15/12 mã hoàn 7% tối đa 200k đơn từ 400K cho sản phẩm hoàn xu Xtra. List sản phẩm áp mã: https://tinyurl.com/2jvx7bnr
+► Lưu trước trên banner tại link này https://tinyurl.com/2p9kpsoj + Dùng lúc 0H ngày 15/12 mã hoàn 6% tối đa 100k đơn từ 50k cho sản phẩm hoàn xu Xtra. List sản phẩm áp mã: https://tinyurl.com/2zy6mnmq
+.
+⭐️ 10. Ưu đãi thanh toán bằng ShopeePay: https://tinyurl.com/2q84jd97
+► SPPDEC15EB500K giảm 5% tối đa 500k đơn từ 2 triệu
+► SPPDEC15EB10K giảm 10k đơn từ 140k
+► SPPP3NOV8K giảm 8k đơn từ 99k
+► SPPP3NOVGMV40K giảm 5% tối đa 40k đơn từ 400k
+.
+⭐️ 11. Hàng Hiệu Giá Tốt: https://tinyurl.com/2o4mpj2m
+► BMLTM50 giảm 10% tối đa 50K đơn từ 99K
+► BMINC50 giảm 10% tối đa 50K đơn từ 99K
+► BMLTA50 giảm 10% tối đa 50K đơn từ 99K
+► BMLTM300 giảm 10% tối đa 300K đơn từ 499K
+► BMLT300 giảm 10% tối đa 300K đơn từ 499K
+► Voucher ưu đãi khác đơn từ các thương hiệu
+.
+⭐️ 12. Nhóm Điện tử công nghệ: https://tinyurl.com/2e75fjz8
+► 0H: dùng ngay mã giảm 8K đơn từ 99K đã được add vào ví tại đây: https://tinyurl.com/2evqugt4
+.
+📌 12.1. Công Nghệ Mall: https://tinyurl.com/2znufqmg
+► ELRE1TR5 giảm 8% tối đa 1500k đơn từ 5000k
+► ELMALL5 giảm 5% tối đa 500k đơn từ 300k
+► ELRE500 giảm 10% tối đa 500k đơn từ 500k
+► ELBAU4 giảm 4% tối đa 1tr2 đơn từ 500K
+► ELBAU5 giảm 5% tối đa 500K đơn từ 300K
+► ELTO30 giảm 5% tối đa 30K đơn từ 0Đ
+.
+⭐️ 13. Thời trang: https://tinyurl.com/2nzd59ms
+► 0h: dùng ngay mã giảm 8K đơn từ 99K cho Thời trang đã được add vào ví tại đây: https://tinyurl.com/2evqugt4
+🔸 Săn deal 1K, 11K các khung giờ 0H, 9H, 12H, 15H, 18H, 21H
+🔸 Săn mã 440K các khung giờ 15H, 17H, 19H
+🔸 Săn mã 222K các khung giờ 0H, 1H, 9H, 12H, 15H, 18H:
+► 0H: ROUGH220K (mã shop) giảm 220K đơn từ 599k cho sp của thương hiệu ROUGH: https://tinyurl.com/2eoogfl4
+► 0H29s: TORA2222K (mã shop) giảm 220K đơn từ 600k cho sp của shop TORANO Official Store: https://tinyurl.com/2nq6f4mm
+.
+📌 13.1. Voucher Thời Trang: https://tinyurl.com/2g6rk9nh
+► Vào lưu các mã 50K/50K đơn từ các shop thời trang
+► Voucher ưu đãi đơn từ các thương hiệu.
+► Voucher đơn từ các shop bán.
+.
+⭐️ 14. Nhóm 5 ngành Tiêu dùng: https://tinyurl.com/2l85lcul
+(Gồm: Sức khỏe sắc đẹp, Bách hóa online, Mẹ bé, Giặt giũ, Thú cưng)
+▶ 0h: dùng ngay mã giảm 5% tối đa 40K cho đơn từ 250K cho 5 ngành Tiêu dùng đã được add vào ví tại đây: https://tinyurl.com/2evqugt4
+▶ 0h: dùng ngay mã giảm 5% tối đa 100K cho đơn từ 500K cho 5 ngành Tiêu dùng đã được add vào ví tại đây: https://tinyurl.com/2evqugt4
+► 0h: dùng ngay mã giảm 8K đơn từ 99K cho 5 ngành Tiêu dùng đã được add vào ví tại đây: https://tinyurl.com/2evqugt4
+► Lưu voucher đơn từ các thương hiệu, shop bán hàng
+🔸 Săn voucher 99K các khung giờ: 0H, 9H, 12H, 15H, 18H, 21H
+.
+📌 14.1. Tiêu Dùng Mall: https://tinyurl.com/2o26zx8s
+► FMCGMALL giảm 8% tối đa 80k đơn từ 250k
+► FMCGHOT giảm 10% tối đa 90k đơn từ 600k
+► FMCG100K giảm 100k đơn từ 1 triệu
+► COSLUX11 giảm 120k đơn từ 1500k
+► COSSPIKE60K giảm 10% tối đa 60k từ 400k
+► COSSPIKE100K giảm 10% tối đa 100k từ 400k
+► COSBEAUTY25K11 hoàn 25k từ 250k
+► COSSBC50K11 giảm 10% tối đa 50k đơm từ 499k
+► COSSBC100K11 giảm 10% tối đa 100k từ 999k
+► Lưu các mã freeship Shopee Mart
+► Quà tặng cho các đơn hàng.
+► Voucher ưu đãi đơn từ các thương hiệu.
+.
+📌 14.2. Sức Khỏe Sắc Đẹp: https://tinyurl.com/2e8vnhg9
+► Voucher ưu đãi đơn từ các shop bán.
+► Deal đồng giá 1k, x9k, quà tặng 0đ
+.
+📌 14.3. Beauty Club: https://tinyurl.com/2jzsml73
+► COSBEAUTY25K11 hoàn 25K đơn từ 250K
+► COSSBC50K11 giảm 10% tối đa 50k đơn từ 499k
+► COSSBC100K11 giảm 10% tối đa 100k đơn từ 99k
+► Voucher ưu đãi đơn từ các shop bán.
+► Deal đồng giá 1k, x9k, quà tặng 0đ
+.
+📌 14.4. Mẹ bé – Mum Club: https://tinyurl.com/2gnzbfle
+► FMCGMEXMKB11 hoàn 50K đơn từ 499K
+► FMCGMEXTK11 hoàn 15% tối đa 30k đơn từ 99k
+► Voucher ưu đãi đơn từ các thương hiệu.
+.
+⭐️ 15. Nhóm 4 ngành Đời sống : https://tinyurl.com/2pwe7gcc
+(Gồm: Nhà cửa đời sống, Xe & phụ kiện, Đồ chơi, Nhà sách online)
+Các mã bên dưới đã được add sẵn vào ví voucher của bạn rồi nhé. Kiểm tra ngay tại đây: https://tinyurl.com/2evqugt4
+► Mã giảm 8K đơn từ 99K cho 4 ngành Đời Sống
+► LIFE1111A giảm 10% tối đa 10K cho đơn hàng đơn từ 50K
+► LIFE1111B giảm 8% tối đa 100K cho đơn hàng đơn từ 300K
+► LIFEMC11SCDP giảm 12% tối đa 80k đơn từ 250k
+► LIFEMC11SADP giảm 12% tối đa 30k đơn từ 99k
+► LIFEMCSBP1 giảm 12% tối đa 30k đơn từ 99k
+► LIFEMC11SBDP giảm 12% tối đa 50k đơn từ 150k
+► LIFEMCSBP2 giảm 12% tối đa 50k đơn từ 150k
+► LIFEMCSBP4 giảm 10% tối đa 1000k đơn từ 3000k
+► LIFEMCSBP3 giảm 12% tối đa 80k đơn từ 150k
+► Quà tặng 0đ đơn từ các shop bán.
+► Săn deal đồng giá 1k, x9k.
+.
+📌 15.1. Thương Hiệu Đời Sống: https://tinyurl.com/2mpw26s3
+► LIFEMC11A giảm 8% tối đa 30K đơn từ 99K
+► LIFEMC11B giảm 8% tối đa 50K đơn từ 150K
+► LIFEMC11C giảm 8% tối đa 80K đơn từ 350K
+► Voucher ưu đãi đơn từ shop bán, deal x9k
+.
+📌 15.2. Book Club: https://tinyurl.com/2zj2ezfx
+► LIFESBC11 giảm 10% tối đa 15K cho đơn từ 20K
+► LIFESBCWG11 giảm 15% tối đa 20K cho đơn từ 0 đồng
+► Voucher ưu đãi đơn từ các thương hiệu, shop
+► Lưu thêm mã Freeship cho Book Club
+.
+📌 15.3. Home Club – Nội thất: https://tinyurl.com/2zguna2j
+► LIFESHC11WG giảm 20K đơn từ 20K
+► LIFESHC11BAU giảm 10% tối đa 15K đơn từ 50K
+► LIFESHC1130K giảm 15% tối đa 30K đơn từ 99K
+► LIFESHC1120K giảm 15% tối đa 20K đơn từ 50K
+► Lưu thêm mã Freeship 15K, 50K cho HomeClub
+► Voucher ưu đãi đơn từ các shop bán.
+► Săn sale đồng giá 1k, x9k
+.
+⭐️ 16. Hàng Quốc Tế: https://tinyurl.com/2zbpv35p
+► 0H: Mã giảm 5% tối đa 50K đơn từ 150K (mã đã được thêm sẵn trong ví voucher Shopee của bạn rồi). Bạn vào đây để kiểm tra xem mình có mã không nhé: https://tinyurl.com/2evqugt4
+► Lưu voucher đơn từ các thương hiệu, shop bán hàng
+► Săn deal 1k, 9k, x9k đơn từ chương trình
+► 0H: 1212EBVCB25K hoàn 25K đơn từ 150K. List sản phẩm áp mã: https://tinyurl.com/2qqeg33u
+► 0H: CBSPIKE1212 giảm 12% tối đa 50K đơn từ 250K. List sản phẩm áp mã: https://tinyurl.com/2kyrlrh5
+.
+⭐️ 17. Shop xu hướng: https://tinyurl.com/2kuwlml4
+► BMLTM50 giảm 10% tối đa 50k đơn từ 99k
+► INCU300 giảm 10% tối đa 300k đơn từ 499k
+► BMINC50 giảm 10% tối đa 50k đơn từ 99k
+► BMLTM300 giảm 10% tối đa 300k đơn từ 499k
+► BMLTA50 giảm 10% tối đa 50k đơn từ 99k
+► BMLT300 giảm 10% tối đa 300k đơn từ 499k
+► BMLT100 giảm 7% tối đa 100k đơn từ 499k
+► 0H: List deal đồng giá 1K: https://tinyurl.com/2pgy3ewr
+► Voucher, ưu đãi đơn từ các thương hiệu
+.
+⭐️ 18. Shop lên sàn mới: https://tinyurl.com/2q7yb36w
+► 0H: SRDEC12 giảm 50% tối đa 20K đơn từ 0Đ. List sản phẩm áp mã: https://tinyurl.com/2j6hvmz5
+► 0H: SRDEC12B hoàn 100% tối đa 20K đơn từ 50K. List sản phẩm áp mã: https://tinyurl.com/2n2nu224
+► SRDECP220 giảm 20k từ 50k
+► SRDECP215 giảm 50%, max 15k từ 0k
+► SRDECTT12 giảm 50%, max 15k từ 0k
+► SRDECTT1 giảm 20k từ 50k
+► SRDECTT2 giảm 50%, max 20k từ 0k
+► Voucher ưu đãi đơn từ các shop bán.
+► Săn deal đồng giá 1k các khung giờ.
 `
 
 var updateTime = new Date(); //updateTime
