ce1c521493a505c62f2e227bab69b61a04ca85ac
Cuongbok <92067448+cuongbokreal@users.noreply.github.com>
2023-05-04 10:07:10 +0700

    Create mgg_ghim.js

---
+++ cuongbokit/ma-giam-gia/tgg/v1/mgg_ghim.js
@@ -0,0 +1,20 @@
+var mgg_ghim = [
+  {
+    value: 'Giảm 20K',
+    minspend: 'Cho đơn từ 100K',
+    link: 'https://promo.highlandscoffee.com.vn/uudai4',
+    start_time: '2023/05/02',
+    end_time: '2023/05/11',
+    remain: 'Không giới hạn',
+    image: 'https://cdn.haitrieu.com/wp-content/uploads/2022/03/highlands-coffee-red-logo.png',
+  },
+  {
+    value: 'Giảm 25K',
+    minspend: 'Cho đơn từ 120K',
+    link: 'https://promo.highlandscoffee.com.vn/uudai4',
+    start_time: '2023/05/02',
+    end_time: '2023/05/11',
+    remain: 'Không giới hạn',
+    image: 'https://cdn.haitrieu.com/wp-content/uploads/2022/03/highlands-coffee-red-logo.png',
+  }
+]
