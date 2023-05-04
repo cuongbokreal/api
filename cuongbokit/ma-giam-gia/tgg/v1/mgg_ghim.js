f21b38b3bc987d24b10717effd382367a68aabeb
Cuongbok <92067448+cuongbokreal@users.noreply.github.com>
2023-05-04 11:19:05 +0700

    Update mgg_ghim.js

--- cuongbokit/ma-giam-gia/tgg/v1/mgg_ghim.js
+++ cuongbokit/ma-giam-gia/tgg/v1/mgg_ghim.js
@@ -3,6 +3,17 @@ link: aff link
 */
 var mgg_ghim = [
   {
+    value: 'Freeship không giới hạn',
+    minspend: 'Toàn sàn',
+    tag: 'Tiki',
+    link: 'https://shorten.asia/vnUpTbVG',
+    start_time: '2023/03/01',
+    end_time: '∞',
+    remain: 'Không giới hạn',
+    image: 'https://salt.tikicdn.com/ts/upload/e4/49/6c/270be9859abd5f5ec5071da65fab0a94.png',
+    detail: 'Từ ngày 01/03/2023, Tiki đồng hành cùng nhà bán hàng ra mắt chương trình Freeship Không Giới Hạn. Cụ thể, giá trị đơn hàng được tính trên tổng giá trị giỏ hàng, không phân biệt của nhà bán nào. Giỏ hàng lúc thanh toán của bạn chỉ cần đạt giá trị từ 149.000₫ hoặc 299.000₫ sẽ được miễn phí vận chuyển tương ứng 15.000₫ hoặc 30.000₫.',
+  },
+  {
     value: 'Giảm 20K',
     minspend: 'Cho đơn từ 100K',
     tag: 'Highlands',
