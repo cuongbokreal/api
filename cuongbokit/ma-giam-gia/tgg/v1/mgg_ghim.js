5ed74aead50f2387eadbaeb0af7c3b441d74df84
Cuongbok <92067448+cuongbokreal@users.noreply.github.com>
2023-05-08 15:11:16 +0700

    Update mgg_ghim.js

--- cuongbokit/ma-giam-gia/tgg/v1/mgg_ghim.js
+++ cuongbokit/ma-giam-gia/tgg/v1/mgg_ghim.js
@@ -4,6 +4,18 @@ link: aff link
 var mgg_ghim = [
 
   {
+    value: ' Giảm 25K đơn 99K',
+    minspend: 'Toàn sàn',
+    tag: 'Lazada độc quyền',
+    link: 'https://shorten.asia/W8Zxdbv8',
+    start_time: '2023/05/05',
+    end_time: '2023/05/09 23:59',
+    remain: '30',
+    image: ' https://i.imgur.com/gXA8JxH.png ',
+    detail: '   Code 25k/99k toàn sàn độc quyền Trùm Giảm Giá ',
+code: 'LazT525dmnpj8',
+  },
+  {
     value: ' Giảm 20K đơn 99K',
     minspend: ' Thời trang',
     tag: 'Lazada độc quyền',
