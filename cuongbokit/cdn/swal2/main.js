0359aaa28f70ab265de4a02facf4c1b3e2f75316
Cuongbok <92067448+cuongbokreal@users.noreply.github.com>
2023-05-14 09:46:07 +0700

    Create main.js

---
+++ cuongbokit/cdn/swal2/main.js
@@ -0,0 +1,16 @@
+//<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.1.5/dist/sweetalert2.all.min.js"></script>
+/*
+Thư viện Swal2 cung cấp một loạt các tùy chọn để tùy chỉnh hộp thoại cảnh báo. Dưới đây là một số tùy chọn quan trọng:
+
+title: Tiêu đề của hộp thoại cảnh báo.
+text: Nội dung văn bản của hộp thoại cảnh báo.
+icon: Biểu tượng hiển thị trong hộp thoại cảnh báo, ví dụ: 'warning', 'error', 'success', 'info'.
+confirmButtonText: Văn bản của nút xác nhận trong hộp thoại cảnh báo.
+cancelButtonText: Văn bản của nút hủy bỏ trong hộp thoại cảnh báo.
+showCancelButton: Xác định xem nút hủy bỏ có được hiển thị hay không.
+allowOutsideClick: Cho phép người dùng nhấp chuột bên ngoài hộp thoại để đóng nó.
+timer: Thời gian tự động đóng hộp thoại (được tính bằng mili giây).
+onOpen: Hàm được gọi khi hộp thoại mở.
+onClose: Hàm được gọi khi hộp thoại đóng.
+*/
+
