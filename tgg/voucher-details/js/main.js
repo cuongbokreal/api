022e595a72b1e401e8d9a6328bd0c171efcd975f
Cuongbok <92067448+cuongbokreal@users.noreply.github.com>
2023-07-31 22:30:40 +0700

    Update main.js

--- tgg/voucher-details/js/main.js
+++ tgg/voucher-details/js/main.js
@@ -70,15 +70,15 @@ var copyCouponCode = (event, couponCode, link, shouldCopy = false) => {
         window.open(link);
 
         //event.target.childNodes[0].textContent = couponCode;
-      	event.target.textContent = couponCode;
-      
-      //thbao
-      toast({
-        title: 'Thành công',
-        message: `Đã sao chép mã: <b><a rel="nofollow" target="_blank" href="${link}">${couponCode}</a></b>`,
-        type: "success",
-        duration: 1000000000
-      })
+        event.target.textContent = couponCode;
+
+        //thbao
+        toast({
+            title: 'Thành công',
+            message: `Đã sao chép mã: <b><a rel="nofollow" target="_blank" href="${link}">${couponCode}</a></b>`,
+            type: "success",
+            duration: 1000000000
+        })
     }
 }
 
@@ -94,4 +94,66 @@ function copy_code() {
 
 }
 
+function showSuccessToast(title, message) {
+    toast({
+        title: title,
+        message: message,
+        type: "success",
+        duration: 5000
+    });
+}
 
+function showErrorToast(title, message) {
+    toast({
+        title: title,
+        message: message,
+        type: "error",
+        duration: 5000
+    });
+}
+// Toast function
+function toast({
+    title = "",
+    message = "",
+    type = "info",
+    duration = 3000
+}) {
+    const main = document.getElementById("toast");
+    if (main) {
+        const toast = document.createElement("div");
+        // Auto remove toast
+        const autoRemoveId = setTimeout(function() {
+            main.removeChild(toast);
+        }, duration + 1000);
+        // Remove toast when clicked
+        toast.onclick = function(e) {
+            if (e.target.closest(".toast__close")) {
+                main.removeChild(toast);
+                clearTimeout(autoRemoveId);
+            }
+        };
+        const icons = {
+            success: "fas fa-check-circle",
+            info: "fas fa-info-circle",
+            warning: "fas fa-exclamation-circle",
+            error: "fas fa-exclamation-circle"
+        };
+        const icon = icons[type];
+        const delay = (duration / 1000).toFixed(2);
+        toast.classList.add("toast", `toast--${type}`);
+        toast.style.animation = `slideInLeft ease .3s, fadeOut linear 1s ${delay}s forwards`;
+        toast.innerHTML = `
+			                    <div class='toast__icon'>
+			                        <i class='${icon}'></i>
+			                    </div>
+			                    <div class='toast__body'>
+			                        <h3 class='toast__title'>${title}</h3>
+			                        <p class='toast__msg'>${message}</p>
+			                    </div>
+			                    <div class='toast__close'>
+			                        <i class='fas fa-times'></i>
+			                    </div>
+			                `;
+        main.appendChild(toast);
+    }
+}
