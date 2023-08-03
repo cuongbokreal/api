be81bf3bd132fbf2576c0689279b21f97b9b682a
Cuongbok <92067448+cuongbokreal@users.noreply.github.com>
2023-08-03 22:12:46 +0700

    Create nhomSanSale.js

---
+++ tgg/inner/nhomSanSale.js
@@ -0,0 +1,6 @@
+try {
+  document.querySelector('#nhomSanSale').innerHTML = `<p id="kq_text">👉🏻 Nhóm săn sale mọi hàng ngày khung giờ:<br>- Z.alo: <a class="text-link" rel="nofollow" target="_blank" href="https://zalo.me/g/fqehfa279">https://zalo.me/g/fqehfa279</a>  <br>- Tele: <a class="text-link" rel="nofollow" target="_blank" href="https://t.me/trumgiamgia">https://t.me/trumgiamgia</a>  (nhanh hơn 1-2s)</p>`
+}
+catch(err) {
+  console.log('ko co elm nhom san sale')
+}
