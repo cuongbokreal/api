e4a03bbb80b30012d5870a53b7cb4dc791c9f916
Cuongbok <92067448+cuongbokreal@users.noreply.github.com>
2023-10-13 19:37:53 +0700

    Update main.js

--- cuongbokit/tien-ich/khung-chua-code/main.js
+++ cuongbokit/tien-ich/khung-chua-code/main.js
@@ -8,3 +8,16 @@ function copyToClipboard(elmQuery) {
         document.body.removeChild(tempTextArea);
         console.log('Nội dung đã được sao chép vào clipboard.');
 }
+function selectAllText() {
+    const contentContainer = document.querySelector('.content-container');
+
+    // Kiểm tra xem có nội dung nào trong khung chứa không
+    if (contentContainer) {
+        const range = document.createRange();
+        range.selectNodeContents(contentContainer);
+
+        const selection = window.getSelection();
+        selection.removeAllRanges();
+        selection.addRange(range);
+    }
+}
