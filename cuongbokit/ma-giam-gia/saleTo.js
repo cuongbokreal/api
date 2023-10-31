5e0cb2d4b40f5b14b19e374a87d10a3e3c13c3e5
Cuongbok <92067448+cuongbokreal@users.noreply.github.com>
2023-10-31 22:48:53 +0700

    Update saleTo.js

--- cuongbokit/ma-giam-gia/saleTo.js
+++ cuongbokit/ma-giam-gia/saleTo.js
@@ -1,11 +1,137 @@
 var content_dday = `
+⭐️ 1. Săn Siêu Voucher 1.11: https://tinyurl.com/yp9h2v3a 
+► 0H: ĐÃ COPY! giảm 50% tối đa 5tr đơn từ 5tr 
+► 0H: FAHUNT1NOV giảm 200K đơn từ 0Đ áp dụng ngành hàng Thời trang (Back lượt lúc 9H, 12H, 15H, 18H, 21H)
+► 0h: FMCGHUNT1110 giảm 111k đơn từ 0đ 
+.
+⭐️ 2. Săn Voucher tại banner KOL Shopee: 
+🔔 2.1. Voucher lưu và dùng luôn ngày 31/10:  https://tinyurl.com/ymydlxzm  
+►19H, 21H, 23H: Lưu trên banner + dùng ngay mã giảm 100% tối đa 50K cho đơn từ 150K toàn sàn Shopee
+ 
+🔔 2.2. Voucher Hot các khung giờ trong ngày 1/11 lưu tại link này: https://tinyurl.com/ymydlxzm  
+📌Voucher toàn sàn Shopee:
+► 0H, 12H: Lưu trên banner + dùng ngay mã giảm 50% tối đa 500K đơn từ 500K toàn sàn Shopee
+► 0H, 12H: Lưu trên banner + dùng ngay mã giảm 20% tối đa 50K đơn từ 99K toàn sàn Shopee
+► 9H, 15H, 18H, 21H: Lưu trên banner + dùng ngay mã giảm 20% tối đa 50K đơn từ 99K toàn sàn Shopee (lọc user)
+.
+📌Voucher Thời trang:
+► 0H: Lưu trên banner + dùng ngay mã giảm 20% tối đa 60K đơn từ 150K cho Thời trang 
+► 9H, 12H, 15H, 18H, 21H: Lưu trên banner + dùng ngay mã giảm 20% tối đa 60K đơn từ 150K cho Thời trang (lọc user)
+.
+📌Voucher Sức khỏe & Làm đẹp:
+► 0H: Lưu trên banner + dùng ngay mã giảm 20% tối đa 60K đơn từ 150K cho Sức khỏe & Sắc đẹp
+► 9H, 12H, 15H, 18H, 21H: Lưu trên banner + dùng ngay mã giảm 20% tối đa 60K đơn từ 150K cho Sức khỏe & Sắc đẹp (lọc user)
+.
+📌Voucher Thương hiệu:
+► 0H: Lưu trên banner + dùng ngay mã giảm 20% tối đa 150K cho đơn từ 500K (Áp dụng cho 5 nhãn hàng: Samsung IM, Huawei, Philips, Tecno, ELMICH)
+►0H: Lưu trên banner + dùng ngay mã giảm 50% tối đa 50K cho đơn từ 99K (Áp dụng cho 15 nhãn hàng: adidas, Teelab, Canifa, Joymall, ONOFF, Karants, Bitis, Fonterra, enchanteur_vietnam, johnson_johnson_official, INNISFREE, LIXIBOX, Eucerin, MELANO CC, SUNPLAY)
+.
+⭐️ 3. Điện tử công nghệ: https://tinyurl.com/yvvxbhlz  
+►0H: ELTTS15 giảm 10k đơn từ 10k
+►0H, 9H, 12H, 21H: ELTECH1T giảm 10%, tối đa 1,1tr đơn từ 1tr
+► Voucher ưu đãi từ các brand công nghệ.
+.
+⭐️ 4. Thời trang: https://tinyurl.com/ysk8noce  
+📌 Săn voucher 200k các khung giờ:
+► 0H: FAHUNT1NOV giảm 200K đơn từ 0Đ áp dụng ngành hàng Thời trang (Back lượt lúc 9H, 12H, 15H, 18H, 21H)
+► 0H, 0H30, 1H, 1H30, 9H, 12H, 15H, 18H, 21H: Săn deal đồng giá 1k, x9k các khung giờ.
+ 
+📌 Săn mã shop giảm đến 220K (mở mới mỗi khung giờ 0H, 9H, 12H, 15H, 18H):
+► 0h: VINGOH220 giảm 220k đơn từ 600k 
+► 0h: FURL220OS giảm 220k đơn từ 600k
+► Voucher ưu đãi từ các shop bán.
+► Mã Freeship 15K đơn 0đ cho sản phẩm Thời trang (có sẵn trong ví)
+.
+⭐️5. Voucher Xtra:  https://tinyurl.com/ym5kqpxv  
+►0h, 9h, 12h, 15h, 18h: Lưu trên banner + dùng ngay  mã giảm 15% tối đa 150k đơn từ 500k cho sản phẩm có gắn tag Xtra ( 9h, 12h, 18h là mã back lượt)
+►0h, 9h, 12h, 15h, 18h: Lưu trên banner + dùng ngay mã giảm 15% tối đa 100k đơn từ 120k toàn sàn cho sản phẩm có gắn tag Xtra  ( 9h, 12h, 18h là mã back lượt) 
+ 
+⭐️6. Mã nhập tay HOT:
+► 0H: AFFGMZG0 giảm 30k đơn từ 99K (lọc user)
+► 0H: AFFCHUOI giảm 30k đơn từ 99K 
+► 0H: AFFTUI giảm 50% tối đa 30k đơn từ 50K 
+► 0H: AFFCHAY giảm 30% tối đa 30k đơn từ 68K 
+► 0H: AFFNGAY giảm 20% tối đa 50k đơn từ 150K 
+► 0H: AFFBAT giảm 20% tối đa 68k đơn từ 299K 
+► 0H: AFFDAU giảm 20% tối đa 88k đơn từ 399K 
+► 0H: AFFTU giảm 15% tối đa 111k đơn từ 499K 
+► 0H: AFFTU giảm 15% tối đa 211k đơn từ 899K 
+► 0H: AFFDAI giảm 15% tối đa 68k đơn từ 199K 
+.
+⭐️7. Các voucher sẽ có trong Livestream:
+🔔7.1. Voucher có sẵn trong ví: https://tinyurl.com/yufwt878  
+► 0H: Mã freeship 70K đơn 25K, freeship 25K đơn 25K toàn sàn 💢
+► 0H: Mã giảm 60K đơn từ 400K toàn sàn Shopee 💢
+► 0H: Mã giảm 50%, 40%, 30% tối đa 25K đơn từ 0Đ toàn sàn Shopee (Tùy tài khoản) 💢
+► 0H: Mã giảm 50% tối đa 25K đơn từ 0Đ toàn sàn Shopee 💢
+► 0H: Mã giảm 15% tối đa 70k đơn từ 100k có sẵn trong ví voucher 
+► 0H: Mã giảm 15% tối đa 50k đơn từ 250k cho thời trang & làm đẹp có sẵn trong ví voucher
+► 0H: Mã giảm 50K đơn từ 250K toàn sàn Shopee (lọc user)
+► 0H: Mã giảm 15% tối đa 40k đơn từ 100k cho thời trang, làm đẹp có sẵn trong ví voucher 
+► 0H: Mã giảm 50% tối đa 25k đơn 0đ cho Thời trang & Làm đẹp
+► 0H: Mã giảm 50% tối đa 25k đơn 0đ cho Thời trang, List áp dụng => https://tinyurl.com/yoybw8sp  
+.
+⭐️ 8. Nhóm 4 ngành Đời sống: https://tinyurl.com/yrpra9zd  
+(Gồm: Nhà cửa đời sống, Xe & phụ kiện, Đồ chơi, Nhà sách online)
+📌 Săn voucher 50k, 100k, 200k các khung:
+► 0h: LIFE111A giảm 50k đơn từ 200k
+► 12h: LIFE111B giảm 100k đơn từ 500k
+► 18h: LIFE111C giảm 200k đơn từ 1000k
+► 0h: LIFE1NOV15K giảm 15k đơn từ 99k
+► 0h: LIFE1NOV100K giảm 12% tối đa 100k đơn từ 500k
+► 0h: LIFE1NOV111K giảm 111k đơn từ 500k
+► Săn deal đồng giá x9k, quà 0đ
+.
+⭐️ 9. Nhóm 5 ngành Tiêu dùng: https://tinyurl.com/yu94hlxg  
+(Gồm: Sức khỏe & Làm đẹp, Mẹ & Bé, Bách hóa, Giặt giũ nhà cửa, Thú cưng)
+► 0h: FMCGHUNT1110 giảm 111k đơn từ 0đ 
+► 0H: Lưu trên banner + dùng ngay mã giảm 30k đơn từ 99k
+► 0H: Lưu trên banner + dùng ngay mã giảm 100k đơn từ 500k
+► 0H: Lưu trên banner + dùng ngay mã giảm 10% tối đa 100k đơn từ 200k
+.
+⭐10. Hàng Quốc Tế: https://tinyurl.com/ypn7q5gr   
+► 0H: CBMINI114 giảm 100k đơn từ 300k
+► Săn deal đồng giá x9k.
+.
+⭐️11. Shop Xu Hướng: https://tinyurl.com/yl4bynx5  
+► Lưu trước trên banner + dùng ngay lúc 0H mã giảm 99K đơn từ 399k (mã shop) 
+► Săn deal đồng giá x9k
+.
+⭐️12. Shop Mới Lên Sàn: https://tinyurl.com/yof2mg99  
+► NSF110150 giảm 50k đơn từ 149k
+► NSF110140 giảm 40k đơn từ 129k
+► NSF11P130 giảm 30k đơn 85k
+► NSF11P125 giảm 25k đơn 70k
+► NSF11P1FS giảm 25k đơn 50k
+► NSF11P1HB giảm 25k đơn 50k
+.
+🚛 13. Chính sách miễn phí vận chuyển 01/11:
+►0h, 9h, 12h, 15h, 18h, 21h: Lưu mã Freeship giảm 15k đơn từ 0đ tại đây: https://tinyurl.com/yp9h2v3a  
+► Ngoài ra, Shopee tặng bạn thêm mã miễn phí vận chuyển đã có sẵn trong ví voucher cho ngày 1/11 gồm:
+- 3 mã freeship 70k đơn từ 25K cho toàn sàn ShopeeLive
+- 3 mã freeship 25k đơn từ 25K cho toàn sàn ShopeeLive
+- 10 mã freeship 300K đơn từ 45K cho sản phẩm Freeship Xtra
+- 5 mã freeship 300K đơn từ 0đ cho sản phẩm Freeship Xtra
+- 10 mã freeship 300K đơn từ 0đ áp dụng cho ngành hàng Thời trang
+- 5 mã freeship 100K đơn từ 100k cho sản phẩm Freeship Xtra (chỉ ship Hỏa Tốc)
+- 5 mã freeship 30K đơn từ 100k cho sản phẩm Freeship Xtra (chỉ ship Hỏa Tốc)
+- 4 mã freeship 25k đơn từ 50K, chỉ áp dụng cho một số người bán tham gia chương trình Voucher Xtra, Freeship Xtra, Shopee Mall.
+- 2 mã freeship 25K đơn từ 200K cho sản phẩm Freeship Xtra
+- 2 mã freeship 15K đơn từ 50K cho sản phẩm Freeship Xtra
+- 1 mã freeship 55K đơn từ 300K cho sản phẩm Freeship Xtra
+.
+⭐️14. Ưu đãi đối tác: https://tinyurl.com/yknkbfjl  
+► Săn ưu đãi từ các đối tác
+.
+⭐️15. Quà tặng Shopee:  https://tinyurl.com/ypu34pnj  
+► Thu thập vật phẩm đổi 10k xu
 `
 
 var updateTime = new Date(); //updateTime
 var mgg_dday = document.querySelector('#mgg_dday')
 thisDate = updateTime.getDate();
 thisMonth = (updateTime.getMonth() + 1)
-var dday = 0
+var dday = 0;
 if(thisDate == thisMonth){dday = thisDate}else //cùng ngày
 if(thisDate + 1 == thisMonth){dday = thisMonth}else //trước 1 ngày
 if(thisDate > 0 && thisDate <= 15){dday = 15}else //15 hàng tháng
