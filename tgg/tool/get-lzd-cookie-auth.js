1b8f5136995b14ebe3ced3661b2c7d740baf684a
Cuongbok <92067448+cuongbokreal@users.noreply.github.com>
2023-11-24 16:06:53 +0700

    Create get-lzd-cookie-auth.js

---
+++ tgg/tool/get-lzd-cookie-auth.js
@@ -0,0 +1,53 @@
+document.body.innerHTML = '<div id="cookie"></div>' + document.body.innerHTML;
+document.querySelector('#cookie').innerHTML = getCookie('lzd_cid')
+
+
+//JS Cookie by Cuongbok, (c) 2022
+//https://cuongbokreal.github.io/api/web/js/cookie.js
+function setCookie(name,value,days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days*24*60*60*1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+}
+
+function getCookie(name) {
+    var dc = document.cookie;
+    var prefix = name + "=";
+    var begin = dc.indexOf("; " + prefix);
+    if (begin == -1) {
+        begin = dc.indexOf(prefix);
+        if (begin != 0) return null;
+    }
+    else
+    {
+        begin += 2;
+        var end = document.cookie.indexOf(";", begin);
+        if (end == -1) {
+        end = dc.length;
+        }
+    }
+    // because unescape has been deprecated, replaced with decodeURI
+    //return unescape(dc.substring(begin + prefix.length, end));
+    return decodeURI(dc.substring(begin + prefix.length, end));
+}
+/*
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
+*/
+
+function eraseCookie(name) {   
+    document.cookie = name +'=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+}
+console.log('CuongbokIT Cookie import successful!')
