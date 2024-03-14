eca47a2d2d874d92b457606c3e0ed3a344249aed
Cuongbok <92067448+cuongbokreal@users.noreply.github.com>
2024-03-14 19:29:32 +0700

    Update saleTo.js

--- cuongbokit/ma-giam-gia/saleTo.js
+++ cuongbokit/ma-giam-gia/saleTo.js
@@ -1,4 +1,346 @@
 var content_dday = `
+<h2>SHOPEE</h2>
+⭐️1. VOUCHER KOL:  
+👉Ngày 15/03 sẽ có thêm hơn 100+ link có mã toàn sàn Shopee từ các KOL. Mình sẽ chỉ đăng các link này trên kênh FB tại đây: https://www.facebook.com/groups/584602429633228/  hoặc kênh telegram tại đây: https://t.me/trumgiamgia  
+.
+⭐️ 2. VOUCHER KOL: https://shope.ee/40IvaDWQbQ  
+👉 2.1. Voucher lưu và dùng ngày 14/03:  https://shope.ee/40IvaDWQbQ  
+► 21h21: Lưu trên banner + dùng ngay mã giảm 50% tối đa 111k đơn từ 111k
+► 22h22: Lưu trên banner + dùng ngay mã giảm 50% tối đa 222k đơn từ 222k
+► 23h23: Lưu trên banner + dùng ngay mã giảm 50% tối đa 333k đơn từ 333k
+.
+👉 2.2. Voucher lưu trước giờ G lưu tại link này: https://shope.ee/40IvaDWQbQ  
+📌 Voucher được lưu từ 0H-24H ngày 14/03 và dùng lúc 0H ngày 15/03:
+► 21h21: Lưu trên banner + dùng lúc 0H ngày 15/03 mã giảm 50% tối đa 111k đơn từ 111k
+► 22h22: Lưu trên banner  +  dùng lúc 0H ngày 15/03 mã giảm 50% tối đa 222k đơn từ 222k
+► 23h23: Lưu trên banner +  dùng lúc 0H ngày 15/03 mã giảm 50% tối đa 333k đơn từ 333k
+► Mã giảm 15% tối đa 100K đơn từ 500K toàn sàn Shopee
+► Mã giảm 15% tối đa 50K đơn từ 250K toàn sàn Shopee
+► Mã giảm 15% tối đa 15K đơn từ 99K toàn sàn Shopee
+.
+👉 2.3. Voucher hot các khung giờ lưu tại link: https://shope.ee/40IvaDWQbQ  
+► 0H, 12H: Lưu trên banner + dùng ngay mã giảm 50% tối đa 1,333K đơn từ 1,333K toàn sàn 
+► 0H, 0H30, 1H, 1H30: Lưu trên banner + dùng ngay mã giảm 18% tối đa 40K đơn 200K toàn sàn
+► 9H, 12H, 15H, 18H, 21H: Lưu trên banner + dùng ngay mã giảm 18% tối đa 40K đơn 200K toàn sàn (lọc User)
+► 0H, 0H30, 1H, 1H30: Lưu trên banner + dùng ngay mã giảm 10% tối đa 70K đơn 300K toàn sàn
+► 9H, 12H, 15H, 18H, 21H: Lưu trên banner + dùng ngay mã giảm 10% tối đa 70K đơn 300K toàn sàn (lọc User)
+► 0H, 9H, 12H, 15H, 18H, 21H: Lưu trên banner + dùng ngay mã giảm 20% tối đa 150K đơn 350K toàn sàn
+.
+👉 2.4. Voucher cho các thương hiệu (Lọc User)
+► 0H: Lưu trên banner + dùng ngay mã giảm 50% tối đa 50K cho đơn từ 99K 
+👉Áp dụng cho cho 23 nhãn hàng sau: pg_officialstorevn, EUCERIN GIAN HÀNG CHÍNH HÃNG, Thiên Long Official Store, hadalabo.officialstore, TORANO Official Store, Rohto Mentholatum Viet Nam, P&G Giặt Xả Vải, CJ InnerB, Elmich Việt Nam Chính Hãng, SUNHOUSE GROUP JSC, VINGO Official Store, Dyson Official Store, Tefal VN Official, adidas Official Store, sunplay.officialstore, DUREX CHÍNH HÃNG, veet.officialstore, BITI'S OFFICIAL STORE, Enfa Official Store, ONOFF Official Store, Sanofi Consumer Healthcare, olay_officialstorevn, dettol.officialstore
+.
+⭐️3. KHUNG GIỜ SĂN SALE: https://shope.ee/3VMjuzOt3Q  
+► 0H, 12H: Lưu trên banner + dùng ngay mã giảm 50% tối đa 1,333K đơn từ 1,333K toàn sàn 
+► 0h: FAHUNT1503 giảm 200K đơn từ 0Đ áp dụng ngành hàng thời trang (Back lượt lúc 9H, 12H, 15H, 18H, 21H)
+► 0h: 153EBV100K giảm 15%, tối đa 100k đơn từ 500k
+► 0h: 153EBV15K giảm 15%, tối đa 15k đơn từ 99k
+► 0h: 153EBV50K giảm 15%, tối đa 50k đơn từ 250k áp cho shopmall
+.
+⭐️4. SHOPEE LIVE:
+🔔4.1. Voucher trong live AD: https://shope.ee/2VUCj7TMeK  
+► 0H: Mã giảm 30% tối đa 50K đơn từ 150K toàn sàn 
+► 0H: Mã giảm 20K đơn từ 50K toàn sàn
+*Lưu ý các mã trên chỉ lên lúc 0H ngày 15/03 và không back lượt 
+.
+🔔4.2. Voucher có sẵn trong ví: https://shope.ee/2fnXzlSZDk  
+📌Voucher áp gần toàn sàn:
+► 0H: Mã giảm 50% tối đa 25K đơn 0Đ áp cho sản phẩm có tag live Xtra (Mã back lượt lúc 11H, 12H, 20H, 22H)
+► 0H: Mã giảm 20% tối đa 50k đơn từ 100k áp cho sản phẩm có tag live Xtra (Mã back lượt lúc 11H, 12H, 20H, 22H)
+► 0H: Freeship giảm 15k đơn 25k (Back lượt lúc 11H, 12H, 20H, 22H)
+► 0H: Mã giảm 30% tối đa 20K đơn 0Đ áp cho sản phẩm của shop live Xtra
+► 0H: Mã giảm 20% tối đa 25K đơn 0Đ áp cho sản phẩm của shop live Xtra
+► 0H: Mã giảm 15% tối đa 50K đơn 100K áp cho sản phẩm của shop live Xtra
+► 0H: Mã giảm 10% tối đa 300K đơn 300K áp cho sản phẩm của shop live Xtra
+ 
+📌Voucher riêng cho ngành Thời trang & Làm đẹp:
+► 0H: Mã giảm 12% tối đa 100K đơn 250K áp cho thời trang & làm đẹp cho sản phẩm của shop Live Xtra
+► 0H: Mã giảm 12% tối đa 25K đơn 0Đ áp cho thời trang
+.
+⚠️Lưu ý: Muốn áp được các voucher trong livestream thì điều kiện đầu tiên là sản phẩm cần phải được gắn tag ShopeeLive. 
+.
+📌Đặt lịch hẹn với Nô tì để lấy sản phẩm cần mua trong live: None 
+.
+👉Xem hướng dẫn đặt lịch hẹn giới thiệu sản phẩm tại: None  
+.
+⭐️5. MÃ NHẬP TAY AFF: 
+📌Mã đã có hiệu lực: 
+► AFFGMZN9 giảm 20k đơn từ 68k
+► AFFUNTS5 giảm 30k đơn từ 88k
+► AFFUNNC6 giảm 50% tối đa 30k đơn từ 50k
+.
+📌Mã áp gần toàn sàn có hiệu lực lúc 0H ngày 15/03
+►0H:  AFFGMQC2 giảm 15k đơn từ 50k 
+►0H: AFFGMCC8 giảm 25k đơn từ 68k  
+►0H: AFFGMNO0 giảm 25% tối đa 33k đơn từ 99k 
+►0H: AFFGMIY1  giảm 20% tối đa 70k đơn từ 250k 
+►0H: AFFUNIF5  giảm 20% tối đa 88k đơn từ 350k
+►0H: AFFUNEH6  giảm 15% tối đa 150k đơn từ 750k
+►0H: AFFUNTZ1  giảm 30k đơn từ 90k 
+►0H: AFFUNWR9 giảm 20% tối đa 100k đơn từ 499k
+►0H: AFFUNAS5  giảm 50% tối đa 30k đơn từ 50k 
+.
+⭐️6. VOUCHER XTRA: https://shope.ee/6zwX9iz0bO  
+► 0h: Lưu trước trên banner + dùng ngay lúc 0H mã giảm 15% tối đa 150K đơn từ  500K áp dụng sản phẩm có tag Voucher Xtra (Back lượt lúc 9H, 12H, 18H)
+► 0h: Lưu trước trên banner + dùng ngay lúc 0H mã giảm 15% tối đa 100K đơn từ  120K áp dụng sản phẩm có tag Voucher Xtra (Back lượt lúc 9H, 12H, 18H)
+.
+⭐️7. MÃ ĐIỆN TỬ CÔNG NGHỆ- Mã Lưu tại banner: https://shope.ee/1VbfXJjYwC  
+► 0H: Lưu banner + dùng ngay mã giảm 10% tối đa 1tr đơn từ 0đ cho ngành điện tử (Back lượt lúc 9H, 12H, 21H)
+► Voucher giảm 100k, 200k, 500k đơn từ các brand công nghệ lưu trên banner
+► Voucher giảm 9% tối đa 1.000K cho đơn từ 300K (có sẵn trong ví)
+► Voucher giảm 10% tối đa 3.000K cho đơn từ 1.000K (có sẵn trong ví)
+► Voucher giảm 10% tối đa 1.500K cho đơn từ 300K (có sẵn trong ví)
+► Voucher giảm 10% tối đa 3.000K cho đơn từ 300K (có sẵn trong ví)
+► Voucher giảm 12% tối đa 3.000K cho đơn từ 300K (có sẵn trong ví)
+.
+⭐️ 8. MÃ THỜI TRANG: https://shope.ee/1qEVvtaImi  
+📌 Săn mã 200k các khung giờ:
+► 0h: FAHUNT1503 giảm 200K đơn từ 0Đ áp dụng ngành hàng thời trang (Back lượt lúc 9H, 12H, 15H, 18H, 21H)
+► 0H30, 1H, 1H30, 12H, 15H, 18H, 12H: Săn deal 1k, 9k mở mới mỗi khung 
+► Freeship giảm 15k đơn từ 0đ cho ngành Thời trang (có sẵn trong ví)
+► Mã giảm 10% tối đa 25k đơn từ 99k áp Thời trang (có sẵn trong ví)
+► Mã giảm 12% tối đa 40k đơn từ 250k  áp Thời trang (có sẵn trong ví)
+► Mã giảm 12% tối đa 100k đơn từ 500k  áp Thời trang (có sẵn trong ví)
+ 
+📌 Săn mã shop giảm đến 200K các khung: 
+► 0h: FLORMAR15 (mã shop) giảm 220k đơn từ 600k áp shop Floralpunk Official Store
+► 0h: TORA153 (mã shop) giảm 220k đơn từ 600k áp shop TORANO Official Store
+.
+⭐️9. MÃ ĐỜI SỐNG: https://shope.ee/40J0VuIaG1  
+(Gồm: Nhà cửa đời sống, Xe & phụ kiện, Đồ chơi, Nhà sách online)
+► LIFE1503A giảm 12% tối đa 100K đơn từ 500k
+► LIFE1503B giảm 15k đơn từ 99k
+► Quà tặng 0đ từ các shop bán.
+► Săn deal đồng giá x0k
+► 0h, 9H, 12H, 15H, 18H, 21H: Mở hộp quà săn mã 200k 🔥
+.
+📌 9.1. Book Club: https://shope.ee/5fREUwmAxl  
+► Voucher ưu đãi từ các thương hiệu, shop
+► Lưu thêm mã Freeship cho Book Club
+► Voucher thành viên giảm 12%
+► Sách ôn luyện giảm đến 50%
+.
+📌 9.2. Home Club - Nội thất: https://shope.ee/1qEVvueZZn  
+► Lưu voucher giảm 12% tối đa 50k đơn từ 150k cho Home Club
+.
+⭐10. HÀNG QUỐC TẾ: https://shope.ee/A9tdrEHRRV  
+► CBTHIEN33 giảm 10% tối đa 100k đơn từ 150k
+► CBFA3330 giảm 12% tối đa 30k đơn từ 150k
+► CBFA33100 giảm 12% tối đa 70k đơn từ 300k
+► FACN3330 giảm 12% tối đa 30k đơn từ 99k
+► FACN3340 giảm 12% tối đa 40k đơn từ 150k
+► CBELSP giảm 15% tối đa 500k đơn từ 500k
+► CBELHV giảm 20% tối đa 500k đơn từ 1tr
+📌Săn voucher 100k đơn từ 300k
+► 0H, 12H, 21H: Săn mã giảm 100K đơn từ 300K
+► Voucher ưu đãi từ các shop bán.
+► Săn deal đồng giá x9k lúc 0H, 9H, 12H, 21H.
+.
+⭐️ 11. MÃ TIÊU DÙNG: https://shope.ee/LPi9Ajk35  
+(Gồm 5 ngành hàng: Sức khỏe sắc đẹp, Mẹ & bé, Giặt giũ nhà cửa, Bách hóa, Chăm sóc thú cưng)
+► Voucher giảm 100K cho đơn từ 500K
+► Voucher giảm 12% tối đa 300K cho đơn từ 500K
+► Voucher giảm 12% tối đa 500K cho đơn từ 1.000K
+► Voucher giảm 10% tối đa 150K cho đơn từ 300K
+► Voucher giảm 10% tối đa 50K cho đơn từ 200K
+► Voucher giảm 8% tối đa 100K cho đơn từ 300K
+► Voucher giảm 8% tối đa 100K cho đơn từ 179K
+► Voucher giảm 8% tối đa 50K cho đơn từ 99K
+► Voucher giảm 8% tối đa 100K cho đơn từ 200K
+► Voucher giảm 8% tối đa 100K cho đơn từ 250K
+► Voucher giảm 7% tối đa 70K cho đơn từ 250K
+► Voucher giảm 7% tối đa 50K cho đơn từ 169K
+► Voucher giảm 7% tối đa 70K cho đơn từ 169K
+► Voucher ưu đãi từ các thương hiệu.
+► Săn deal đồng giá 1k, x9k, quà 0đ
+.
+📌 11.1. Sức Khỏe Sắc Đẹp: https://shope.ee/9KKRw1yNrh  
+► Voucher ưu đãi từ các shop bán.
+► Deal đồng giá 1k, x9k, quà tặng 0đ
+.
+📌 11.2. Beauty Club: https://shope.ee/6ANV5sb4LJ  
+► Voucher ưu đãi từ các shop bán.
+► Deal đồng giá 1k, x9k, quà tặng 0đ
+.
+📌 11.3. Mẹ bé – Mum Club: https://shope.ee/9pGnSbEvAi  
+► Voucher ưu đãi từ các thương hiệu. 
+.
+⭐️12. SHOP XU HƯỚNG: https://shope.ee/9pGnSceuol  
+►0H: SMBC153 giảm 12%, tối đa 80k đơn từ 500k
+► 0H: SMBC153 giảm 15%, tối đa 55k đơn từ 350k
+► Voucher ưu đãi từ shop bán.
+► Săn deal đồng giá 1k, 50k.
+► Săn voucher 100k các khung. 🔥
+.
+⭐️13. Siêu sale 15-3:  None  
+► SPPP3MAR8K giảm 8k đơn từ 120k
+► SPPP3MARHB giảm 8% tối đa 30k đơn từ 300k
+► SPPP3MARFA giảm 8% tối đa 20k đơn từ 150k
+► Mã SPayLater giảm tới 100k trong ví.
+► Voucher giảm tới 3m trong ví user.
+► Voucher ưu đãi đơn từ các thương hiệu.
+► Săn deal đồng giá 1k, x9k, mua 1 đc 2.
+► Loạt mã dịch vụ giảm tới 100k
+► Thêm 3 mã MPVC Live 15k/25k
+► Thêm 5 mã MPVC 15k/0đ
+► Thêm nhiều mã 30k, 50k, 300k đơn từy acc.
+.
+⭐️14. Shop lên sàn mới:  https://shope.ee/7pVj4xQ7wh  
+► NSF03P130 giảm 30k đơn từ 99k
+► NSF03P125 giảm 25k đơn từ 55k
+► NSF03P1HB giảm 25k đơn từ 50k
+► NSF03P1FS giảm 25k đơn từ 50k
+► NSF0303 giảm 130k đơn từ 399k
+► NSF030380 giảm 80k đơn từ 249k
+► NSF030350 giảm 50k đơn từ 199k
+► Voucher ưu đãi từ các shop bán.
+► Săn deal đồng giá 1k các khung.
+.
+⭐️15. TỔNG HỢP LIST DEAL HOT SALE 15/03:
+► Hàng hiệu cao cấp Mua 1 tặng 1: https://shope.ee/2LAmWqpSrf  
+► Làm Đẹp Mua 1 tặng 1: https://shope.ee/qLyk4ZIzB  
+► Thời trang Mua 1 tặng 1: https://shope.ee/7AG2Hhc8Om  
+► Deal đồng giá đơn từ 1K: https://shope.ee/4KvquWQFfv  
+► Deal đồng giá đơn từ 9K: https://shope.ee/3L3JifH7s8  
+► Deal đồng giá đơn từ 59K: https://shope.ee/7pVj4viKLg  
+.
+⭐️ 16. CÁC MÃ FREESHIP NGÀY 15/03:
+► 0h, 9h, 12h, 21h: Lưu mã Freeship giảm 15k/0đ, 10k/0đ tại đây: https://shope.ee/8UlPsAfNxL  (mã back lượt)
+► Ngoài ra, Shopee tặng bạn thêm mã miễn phí vận chuyển đã có sẵn trong ví voucher cho ngày 15/03 gồm:
+► Thêm 5 mã miễn phí vận chuyển 15k đơn từ 0đ
+► Thêm 1 mã miễn phí vận chuyển 15k đơn từ 150k
+► Thêm 3 mã miễn phí vận chuyển Xtra tới 300k
+► Thêm 1 mã miễn phí vận chuyển Xtra 15k đơn từ 50k
+► Thêm 1 mã miễn phí vận chuyển Xtra 25k đơn từ 200k
+► Thêm 1 mã miễn phí vận chuyển Xtra 50k đơn từ 300k
+► Thêm 1 mã miễn phí vận chuyển Xtra 55k đơn từ 300k
+► Thêm 5 mã miễn phí vận chuyển hỏa tốc 30k đơn từ 100k
+► Thêm 5 mã miễn phí vận chuyển hỏa tốc 100k đơn từ 100k
+► Thêm 3 mã miễn phí vận chuyển Live 15k đơn từ 25k
+.
+⭐️17. SIÊU SALE 15/03: https://shope.ee/6fJlgnzUky  
+💢 Các mã bên dưới đã được thêm sẵn vào trong ví voucher Shopee của bạn rồi. Bạn vào đây để kiểm tra nhé: https://shope.ee/20Xw8DiL1I   
+.
+📌Mã dành cho thành viên Bạch Kim & Vàng:
+► Voucher giảm 12% tối đa 200K cho đơn từ 750K 
+► Voucher giảm 12% tối đa 250K cho đơn từ 1.000K 
+► Voucher giảm 12% tối đa 3.000K cho đơn từ 5.000K 
+► Voucher giảm 12% tối đa 1.000K cho đơn từ 3.000K 
+.
+📌Mã áp dụng cho tất cả người dùng:
+► Voucher giảm 12% tối đa 100K cho đơn từ 500K 
+► Voucher giảm 8% tối đa 50K cho đơn từ 250K 
+► Voucher giảm 8% tối đa 100K cho đơn từ 750K 
+► Voucher giảm 10% tối đa 200K cho đơn từ 1.000K 
+► Voucher giảm 10% tối đa 500K cho đơn từ 1.500K
+► Voucher giảm 10% tối đa 100K cho đơn từ 500K (chỉ áp dụng cho NBH phía Nam)
+► Voucher giảm 10% tối đa 25K cho đơn từ 99K 
+► Voucher giảm 8% tối đa 20K cho đơn từ 100K 
+► Voucher giảm 10% tối đa 20K cho đơn từ 50K (chỉ áp dụng cho NBH phía Nam)
+► Voucher giảm 12% tối đa 1.000K cho đơn từ 3.000K 
+► Voucher giảm 12% tối đa 3.000K cho đơn từ 5.000K 
+► Voucher giảm 10% tối đa 1.000K cho đơn từ 3.000K 
+► Voucher giảm 10% tối đa 3.000K cho đơn từ 5.000K 
+.
+📌 Ngành Điện Tử: 
+► Voucher giảm 8% tối đa 2.000K cho đơn từ 10.000K 
+► Voucher giảm 9% tối đa 1.000K cho đơn từ 300K 
+.
+📌Ngành Bách Hóa: 
+► Voucher giảm 10% tối đa 80K cho đơn từ 199K 
+.
+📌 Ngành Đời Sống: 
+► Voucher giảm 10% tối đa 400K cho đơn từ 1.500K 
+► Voucher giảm 10% tối đa 30K cho đơn từ 150K 
+► Voucher giảm 10% tối đa 60K cho đơn từ 400K 
+.
+📌 Ngành Thời Trang:
+► Voucher giảm 10% tối đa 25K cho đơn từ 99K 
+► Voucher giảm 12% tối đa 40K cho đơn từ 250K 
+► Voucher giảm 12% tối đa 100K cho đơn từ 500K 
+► Voucher giảm 15% tối đa 30K cho đơn từ 150K 
+► Voucher giảm 15% tối đa 50K cho đơn từ 300K 
+.
+📌 Ngành Sức Khỏe & Sắc Đẹp:
+► Voucher giảm 10% tối đa 40K cho đơn từ 250K 
+► Voucher giảm 12% tối đa 70K cho đơn từ 500K 
+► Voucher giảm 12% tối đa 150K cho đơn từ 750K 
+.
+⭐️ 18. Ưu đãi thanh toán bằng ShopeePay: https://shope.ee/3VMjuzFwxs  
+► Săn deal đồng giá 1k, x9k, mua 1 đc 2.
+► Voucher ưu đãi từ các thương hiệu.
+► 0h: SPPBAYHT153 giảm 100k đơn từ 4tr mua vé máy bay
+► 0h: SPPKSHT153 giảm 50k đơn từ 400k đặt phòng khách sạn
+► 0h: SPPMVHT153 giảm 10% tối đa 50k đơn từ 400k mua vé xem phim 
+► 0h: SPPVINHT153 giảm 20% tối đa 50k đơn từ 400k mua vé VinWonders
+► 0h: SPPVXHT153 giảm 10% tối đa 50k đặt xe 
+.
+⭐19. MỞ QUÀ SĂN XU:
+► Quà tặng: https://shope.ee/50BXhjF70k 
+► Rewards: https://shope.ee/2fncvRF7uy 
+
+<h2>LAZADA</h2>
+
+⭐️Mã toàn sàn trong link EXC (15 - 17/03): https://s.lazada.vn/s.eV5fM?cc  
+📌 Mã thu thập:
+► 0H, 11H, 20H: Mã giảm 25K đơn 99K toàn sàn
+► 0H30, 11H30, 20H30: Mã giảm 25K đơn 99K toàn sàn
+► 0H, 11H, 12H, 12H30, 20H, 20H30: Mã freeship 30K đơn 200K
+.
+⭐️LAZMALL SALE ( Từ 15 - 17/03): https://s.lazada.vn/s.eV5fp?cc  
+► 0H, 11H, 20H: Mã nhập tay giảm 25K đơn 99K
+► 0H30, 11H30, 20H30: Mã nhập tay giảm 50K đơn 250K
+► Mã Freeship giảm 30K đơn 200K
+.
+⭐️TRẠM VOUCHER: https://s.lazada.vn/s.eV5fI?cc  
+► 12H, 20H: Mã thu thập giảm 50K đơn 200K toàn sàn
+► Mã giảm 10% tối đa 400K đơn 1000K
+► Mã giảm 12% tối đa 100K đơn 500K
+► Mã giảm 10% tối đa 50K đơn 250K
+► Mã giảm 8% tối đa 15K đơn 69K
+.
+⭐️LazCoins: https://s.lazada.vn/s.eV5fs?cc  
+► Điểm danh lấy 3000xu đổi voucher + đổi quà giảm tới 50%
+.
+⭐️ LazChoice : https://s.lazada.vn/s.eV5fj?cc  
+► Mua 3 đồng giá chỉ từ 39k + Freeship
+ .
+⭐️ Sale hết dưới 49K: https://s.lazada.vn/s.eV5fr?cc  
+► Lưu Freeship 20K/0đ,  10K/49k áp dụng cho các sản phẩm trong trang khuyến mãi
+► Deal 1K, 9K, 49K
+.
+⭐️ LazFlash Max: https://s.lazada.vn/s.eV5fK?cc  
+► Chương trình flash sale do Lazada trợ giá các sản phẩm bán chạy. Rất nhiều deal siêu tốt với mức trợ giá lên đến 200K. Áp dụng thêm các voucher khác nữa thì giá tốt nhất thị trường luôn
+.
+⭐️Freeship Max: https://s.lazada.vn/s.eV5fq?cc  
+► Freeship toàn quốc 0đ
+► Giảm 5K đơn từ 19K
+► Giảm 20K đơn từ 45K
+► Giảm 30K đơn từ 200K
+► Giảm 60K đơn từ 300K
+.
+⭐️ 0H Lưu Voucher từ các đối tác thanh toán ngân hàng lên mới (9H back lượt): https://s.lazada.vn/s.eV5fo?cc  
+►Giảm 100K đơn từ 1 Triệu cho VIB-Credit
+►Giảm 50K đơn từ 300K cho LazPayLater (FE Credit)
+►Giảm 100K đơn từ 1 Triệu cho Techcombank Debit
+►Giảm 10% đơn từ 200K tối đa 50K cho Techcombank Debit
+►Giảm 100K đơn từ 2 Triệu cho Lazada Citi Platinum của UOB
+►Giảm 500K đơn từ 300K cho Lazada Citi Platinum của UOB
+►Giảm 99K đơn từ 899K cho OCB
+►Giảm 59K đơn từ 500K cho OCB
+►Giảm 80K đơn từ 800K cho VPBank
+►Giảm 50K đơn từ 350K cho HD Saigon
+►Giảm 250K đơn từ 2500K cho Standard Chartered
+►Giảm 150K đơn từ 1500K cho Standard Chartered
+►Giảm 150K đơn từ 1500K cho Sacombank
+.
+⭐️ Lưu mã Zalopay: https://s.lazada.vn/s.eV5fL?cc  
+► Mã giảm 50k đơn từ 999k toàn sàn, Mã giảm 30k đơn từ 599k khi thanh toán bằng ZaloPay
+► Back lượt ngẫu nhiên trong ngày
+.
+⭐️ Lưu mã momo: https://s.lazada.vn/s.eV5fJ?cc  
+► Mã giảm 50k đơn từ 999k toàn sàn, Mã giảm 30k đơn từ 599k khi thanh toán bằng Momo
+► Back lượt ngẫu nhiên trong ngày
 `
 
 var updateTime = new Date(); //updateTime
