9f86c3022df74da3271ce47a1a53db24b40f8025
Cuongbok <92067448+cuongbokreal@users.noreply.github.com>
2025-10-01 17:52:36 +0700

    Create setting.js

---
+++ cuongbokit/tien-ich/slider/setting.js
@@ -0,0 +1,15 @@
+var images = [
+  'https://i.ibb.co/HDzhpkNR/A-nh-chu-p-ma-n-hi-nh-2025-10-01-172135.jpg',
+  'https://i.ibb.co/rKgw4xMv/A-nh-chu-p-ma-n-hi-nh-2025-10-01-172247.jpg',
+  'https://i.ibb.co/Jj0BDxvG/A-nh-chu-p-ma-n-hi-nh-2025-10-01-172621.jpg',
+  'https://i.ibb.co/B2bqYRXq/A-nh-chu-p-ma-n-hi-nh-2025-10-01-172731.jpg',
+  'https://i.ibb.co/jSH5nwq/A-nh-chu-p-ma-n-hi-nh-2025-10-01-172804.jpg',
+  'https://i.ibb.co/bMr47xmK/A-nh-chu-p-ma-n-hi-nh-2025-10-01-172838.jpg',
+  'https://i.ibb.co/qFdtLb8M/A-nh-chu-p-ma-n-hi-nh-2025-10-01-172911.jpg',
+  'https://i.ibb.co/6JJBFdWJ/A-nh-chu-p-ma-n-hi-nh-2025-10-01-172946.jpg',
+  'https://i.ibb.co/0RBgSg40/A-nh-chu-p-ma-n-hi-nh-2025-10-01-173033.jpg',
+  'https://i.ibb.co/XHSd3Ns/A-nh-chu-p-ma-n-hi-nh-2025-10-01-173103.jpg',
+  'https://i.ibb.co/KxYW0NZ3/A-nh-chu-p-ma-n-hi-nh-2025-10-01-173131.jpg',
+  'https://i.ibb.co/DHSHTZzF/A-nh-chu-p-ma-n-hi-nh-2025-10-01-173239.jpg',
+  'https://i.ibb.co/TDWGRmfb/A-nh-chu-p-ma-n-hi-nh-2025-10-01-172026.jpg'
+];
